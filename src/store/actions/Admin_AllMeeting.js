e51b89595fbece5761b6b6a70c82c2c906845e8e
Huzeifa Jahangir <84620132+huzeifajaha@users.noreply.github.com>
2023-02-01 11:40:27 +0500

    change in meeting Filter

--- src/store/actions/Admin_AllMeeting.js
+++ src/store/actions/Admin_AllMeeting.js
@@ -1,13 +1,17 @@
-import { AllMeetingOrganization, deleteOrganizationMeeting, OrganizationMeetingStatus } from "../../commen/apis/Api_config";
+import {
+  AllMeetingOrganization,
+  deleteOrganizationMeeting,
+  getMeetingStatus,
+  OrganizationMeetingStatus,
+} from "../../commen/apis/Api_config";
 import * as actions from "../action_types";
-import axios from "axios"
-import { getAdminURLs } from "../../commen/apis/Api_ends_points";
+import axios from "axios";
+import { getAdminURLs, meetingApi } from "../../commen/apis/Api_ends_points";
 import { RefreshToken } from "./Auth_action";
 
 const allMeetingInit = () => {
   return {
     type: actions.ADMIN_ALLMEETING_INIT,
-
   };
 };
 
@@ -30,8 +34,11 @@ const allMeetingFail = (response, message) => {
 const OrganizationMeetings = (navigate, t) => {
   let userID = localStorage.getItem("userID");
   let token = JSON.parse(localStorage.getItem("token"));
-  let organizationId = localStorage.getItem("organizationID")
-  let data = { OrganizationID: parseInt(organizationId), RequestingUserID: parseInt(userID) }
+  let organizationId = localStorage.getItem("organizationID");
+  let data = {
+    OrganizationID: parseInt(organizationId),
+    RequestingUserID: parseInt(userID),
+  };
 
   return (dispatch) => {
     dispatch(allMeetingInit());
@@ -50,56 +57,91 @@ const OrganizationMeetings = (navigate, t) => {
       .then(async (response) => {
         if (response.data.responseCode === 417) {
           await dispatch(RefreshToken());
+          dispatch(OrganizationMeetings(navigate, t));
         } else if (response.data.responseResult.isExecuted === true) {
-          if (response.data.responseResult.responseMessage === "Admin_AdminServiceManager_AllOrganizationMeetings_01") {
-            await dispatch(allMeetingSuccess(response.data.responseResult.meetings, t("You-are-not-an-admin-Please-contact-support")))
-          } else if (response.data.responseResult.responseMessage === "Admin_AdminServiceManager_AllOrganizationMeetings_02") {
-            await dispatch(allMeetingSuccess(response.data.responseResult.meetings, t("Data-Available")))
-          } else if (response.data.responseResult.responseMessage === "Admin_AdminServiceManager_AllOrganizationMeetings_03") {
-            await dispatch(allMeetingSuccess(response.data.responseResult.meetings, t("No-Data-available-against-this-Organization")))
-          } else if (response.data.responseResult.responseMessage === "Admin_AdminServiceManager_AllOrganizationMeetings_04") {
-            await dispatch(allMeetingSuccess(response.data.responseResult.meetings, t("No-Data-available-against-this-Organization")))
+          if (
+            response.data.responseResult.responseMessage ===
+            "Admin_AdminServiceManager_AllOrganizationMeetings_01"
+          ) {
+            await dispatch(
+              allMeetingSuccess(
+                response.data.responseResult.meetings,
+                t("You-are-not-an-admin-Please-contact-support")
+              )
+            );
+          } else if (
+            response.data.responseResult.responseMessage ===
+            "Admin_AdminServiceManager_AllOrganizationMeetings_02"
+          ) {
+            await dispatch(
+              allMeetingSuccess(
+                response.data.responseResult.meetings,
+                t("Data-Available")
+              )
+            );
+          } else if (
+            response.data.responseResult.responseMessage ===
+            "Admin_AdminServiceManager_AllOrganizationMeetings_03"
+          ) {
+            await dispatch(
+              allMeetingSuccess(
+                response.data.responseResult.meetings,
+                t("No-Data-available-against-this-Organization")
+              )
+            );
+          } else if (
+            response.data.responseResult.responseMessage ===
+            "Admin_AdminServiceManager_AllOrganizationMeetings_04"
+          ) {
+            await dispatch(
+              allMeetingSuccess(
+                response.data.responseResult.meetings,
+                t("No-Data-available-against-this-Organization")
+              )
+            );
           }
         } else {
-
         }
       })
       .catch((response) => {
-        dispatch(allMeetingFail(response.data.responseResult.responseMessage))
+        dispatch(allMeetingFail(response.data.responseResult.responseMessage));
       });
   };
-}
+};
 
 const updateOrganizationMeetingInit = () => {
   return {
-    type: actions.UPDATEORGANIZTIONMEETING_INIT
-  }
-}
+    type: actions.UPDATEORGANIZTIONMEETING_INIT,
+  };
+};
+
 const updateOrganizationMeetingSuccess = (response, message) => {
   return {
     type: actions.UPDATEORGANIZTIONMEETING_SUCCESS,
     response: response,
-    message: message
-  }
-}
+    message: message,
+  };
+};
+
 const updateOrganizationMeetingFail = (message) => {
   return {
     type: actions.UPDATEORGANIZTIONMEETING_FAIL,
-    message: message
-  }
-}
+    message: message,
+  };
+};
+
 const updateOrganizationMeeting = () => {
   let userID = localStorage.getItem("userID");
   let token = JSON.parse(localStorage.getItem("token"));
-  let organizationId = localStorage.getItem("organizationID")
+  let organizationId = localStorage.getItem("organizationID");
 
   let data = {
     OrganizationID: parseInt(organizationId),
     RequestingUserID: parseInt(userID),
     MeetingID: 901,
-    MeetingStatusID: 1
-  }
-  return (async (dispatch) => {
+    MeetingStatusID: 1,
+  };
+  return async (dispatch) => {
     dispatch(updateOrganizationMeetingInit());
     let form = new FormData();
     form.append("RequestMethod", OrganizationMeetingStatus.RequestMethod);
@@ -113,43 +155,43 @@ const updateOrganizationMeeting = () => {
         _token: token,
       },
     })
-      .then((response) => {
-
-      })
-      .catch((response) => {
-      });
-  })
-}
+      .then((response) => {})
+      .catch((response) => {});
+  };
+};
 
 const deleteOrganizationMeetingInit = () => {
   return {
-    type: actions.DELETEORGANIZATIONMEETING_INIT
-  }
-}
+    type: actions.DELETEORGANIZATIONMEETING_INIT,
+  };
+};
+
 const deleteOrganizationMeetingSuccess = (response, message) => {
   return {
     type: actions.DELETEORGANIZATIONMEETING_SUCCESS,
     response: response,
-    message: message
-  }
-}
+    message: message,
+  };
+};
+
 const deleteOrganizationMeetingFail = (message) => {
   return {
     type: actions.DELETEORGANIZATIONMEETING_FAIL,
-    message: message
-  }
-}
+    message: message,
+  };
+};
+
 const deleteOrganiationMessage = (meetingID, MeetingStatusID, t) => {
-  console.log(meetingID, MeetingStatusID, "datadatadata")
+  console.log(meetingID, MeetingStatusID, "datadatadata");
   let userID = localStorage.getItem("userID");
   let token = JSON.parse(localStorage.getItem("token"));
-  let organizationId = localStorage.getItem("organizationID")
+  let organizationId = localStorage.getItem("organizationID");
   let data = {
     OrganizationID: parseInt(organizationId),
     RequestingUserID: parseInt(userID),
     MeetingID: parseInt(meetingID),
-    MeetingStatusID: parseInt(MeetingStatusID)
-  }
+    MeetingStatusID: parseInt(MeetingStatusID),
+  };
   return (dispatch) => {
     dispatch(deleteOrganizationMeetingInit());
     let form = new FormData();
@@ -165,24 +207,126 @@ const deleteOrganiationMessage = (meetingID, MeetingStatusID, t) => {
       },
     })
       .then(async (response) => {
-        console.log(response)
+        console.log(response);
         if (response.data.responseCode === 417) {
           await dispatch(RefreshToken());
         } else if (response.data.responseResult.isExecuted === true) {
-          if (response.data.responseResult.responseMessage === "Admin_AdminServiceManager_DeleteOrganizationMeeting_01") {
-            dispatch(deleteOrganizationMeetingFail(t("You-are-not-an-admin-Please-contact-support")))
-          } else if (response.data.responseResult.responseMessage === "Admin_AdminServiceManager_DeleteOrganizationMeeting_02") {
-            dispatch(deleteOrganizationMeetingSuccess(response.data.responseResult, t("Meeting-Deleted-Successfully")))
-            dispatch(OrganizationMeetings())
-          } else if (response.data.responseResult.responseMessage === "Admin_AdminServiceManager_DeleteOrganizationMeeting_03") {
-            dispatch(deleteOrganizationMeetingFail(t("Meeting-not-Deleted-Successfully")))
+          if (
+            response.data.responseResult.responseMessage ===
+            "Admin_AdminServiceManager_DeleteOrganizationMeeting_01"
+          ) {
+            dispatch(
+              deleteOrganizationMeetingFail(
+                t("You-are-not-an-admin-Please-contact-support")
+              )
+            );
+          } else if (
+            response.data.responseResult.responseMessage ===
+            "Admin_AdminServiceManager_DeleteOrganizationMeeting_02"
+          ) {
+            dispatch(
+              deleteOrganizationMeetingSuccess(
+                response.data.responseResult,
+                t("Meeting-Deleted-Successfully")
+              )
+            );
+            dispatch(OrganizationMeetings());
+          } else if (
+            response.data.responseResult.responseMessage ===
+            "Admin_AdminServiceManager_DeleteOrganizationMeeting_03"
+          ) {
+            dispatch(
+              deleteOrganizationMeetingFail(
+                t("Meeting-not-Deleted-Successfully")
+              )
+            );
           }
-
         }
       })
       .catch((response) => {
-        console.log(response)
+        console.log(response);
       });
   };
-}
-export { deleteOrganiationMessage, OrganizationMeetings, updateOrganizationMeeting };
+};
+
+const getMeetingStatusInit = () => {
+  return {
+    type: actions.ADMIN_MEETINGSTATUS_INIT,
+  };
+};
+
+const getMeetingStatusSuccess = (response, message) => {
+  return {
+    type: actions.ADMIN_MEETINGSTATUS_SUCCESS,
+    response: response,
+    message: message,
+  };
+};
+
+const getMeetingStatusFail = (message) => {
+  return {
+    type: actions.ADMIN_MEETINGSTATUS_FAIL,
+    message: message,
+  };
+};
+
+const GetMeetingStatus = (t) => {
+  let token = JSON.parse(localStorage.getItem("token"));
+
+  return (dispatch) => {
+    dispatch(getMeetingStatusInit());
+    let form = new FormData();
+    form.append("RequestMethod", getMeetingStatus.RequestMethod);
+    axios({
+      method: "post",
+      url: meetingApi,
+      data: form,
+      headers: {
+        _token: token,
+      },
+    })
+      .then(async (response) => {
+        if (response.data.responseCode === 417) {
+          await dispatch(RefreshToken());
+          dispatch(GetMeetingStatus(t));
+        } else if (response.data.responseResult.isExecuted === true) {
+          if (
+            response.data.responseResult.responseMessage ===
+            "Meeting_MeetingServiceManager_GetMeetingStatus_01"
+          ) {
+            await dispatch(
+              getMeetingStatusSuccess(
+                response.data.responseResult.meetingStatus,
+                t("Record-found")
+              )
+            );
+          } else if (
+            response.data.responseResult.responseMessage ===
+            "Meeting_MeetingServiceManager_GetMeetingStatus_02"
+          ) {
+            await dispatch(getMeetingStatusFail(t("No-records-found")));
+          } else if (
+            response.data.responseResult.responseMessage ===
+            "Meeting_MeetingServiceManager_GetMeetingStatus_03"
+          ) {
+            await dispatch(getMeetingStatusFail(t("Somthing-went-worng")));
+          } else if (
+            response.data.responseResult.responseMessage ===
+            "Admin_AdminServiceManager_AllOrganizationMeetings_04"
+          ) {
+            await dispatch(getMeetingStatusFail(t("Somthing-went-worng")));
+          }
+        } else {
+        }
+      })
+      .catch((response) => {
+        dispatch(getMeetingStatusFail(t("Somthing-went-worng")));
+      });
+  };
+};
+export {
+  deleteOrganiationMessage,
+  OrganizationMeetings,
+  updateOrganizationMeeting,
+  GetMeetingStatus,
+};
--- src/store/reducers/Admin_reducer.js
+++ src/store/reducers/Admin_reducer.js
@@ -19,8 +19,8 @@ const initialState = {
   DeleteOrganizationMeetingResponse: null,
   DeleteOrganizationMessageResponseMessage: "",
   UpdateOrganizationMeetingResponse: null,
-  UpdateOrganizationMessageResponseMessage: ""
-
+  UpdateOrganizationMessageResponseMessage: "",
+  AllMeetingsStatus: [],
 };
 
 const adminReducer = (state = initialState, action) => {
@@ -54,24 +54,24 @@ const adminReducer = (state = initialState, action) => {
       return {
         ...state,
         Loading: true,
-        AllOrganizationResponseMessage: ""
+        AllOrganizationResponseMessage: "",
       };
 
     case actions.ADMIN_ALLMEETING_SUCCESS:
-      console.log(action, "statestatemeeting")
+      console.log(action, "statestatemeeting");
       return {
         ...state,
         Loading: false,
         AllOrganizationMeeting: action.response,
-        AllOrganizationResponseMessage: action.message
+        AllOrganizationResponseMessage: action.message,
       };
 
     case actions.ADMIN_ALLMEETING_FAIL:
-      console.log(action, "statestatemeeting")
+      console.log(action, "statestatemeeting");
       return {
         ...state,
         Loading: false,
-        AllOrganizationResponseMessage: action.message
+        AllOrganizationResponseMessage: action.message,
       };
 
     //action Case For Admin-CancelSub
@@ -131,7 +131,7 @@ const adminReducer = (state = initialState, action) => {
     case actions.ADMIN_ORGANIZATION_INIT:
       return {
         ...state,
-        OrganizationCheckSpinner: true
+        OrganizationCheckSpinner: true,
       };
 
     case actions.ADMIN_ORGANIZATION_SUCCESS:
@@ -140,7 +140,7 @@ const adminReducer = (state = initialState, action) => {
         ...state,
         ResponseMessage: action.message,
         OrganisationCheck: action.response,
-        OrganizationCheckSpinner: false
+        OrganizationCheckSpinner: false,
       };
 
     case actions.ADMIN_ORGANIZATION_FAIL:
@@ -150,13 +150,13 @@ const adminReducer = (state = initialState, action) => {
         ...state,
         ResponseMessage: action.message,
         OrganisationCheck: action.response,
-        OrganizationCheckSpinner: false
+        OrganizationCheckSpinner: false,
       };
     //action Case For Admin-Email
     case actions.ADMIN_EMAILVARIFICATION_INIT:
       return {
         ...state,
-        EmailCheckSpinner: true
+        EmailCheckSpinner: true,
       };
 
     case actions.ADMIN_EMAILVARIFICATION_SUCCESS:
@@ -165,7 +165,7 @@ const adminReducer = (state = initialState, action) => {
         ...state,
         ResponseMessage: action.message,
         EmailCheck: action.response,
-        EmailCheckSpinner: false
+        EmailCheckSpinner: false,
       };
 
     case actions.ADMIN_EMAILVARIFICATION_FAIL:
@@ -173,57 +173,57 @@ const adminReducer = (state = initialState, action) => {
         ...state,
         ResponseMessage: action.message,
         EmailCheck: action.response,
-        EmailCheckSpinner: false
+        EmailCheckSpinner: false,
       };
     // actionc ase for admin meeting update
     case actions.UPDATEORGANIZTIONMEETING_INIT: {
       return {
         ...state,
-        Loading: true
-      }
+        Loading: true,
+      };
     }
     case actions.UPDATEORGANIZTIONMEETING_SUCCESS: {
       return {
         ...state,
         Loading: false,
         UpdateOrganizationMeetingResponse: action.response,
-        UpdateOrganizationMessageResponseMessage: action.message
-      }
+        UpdateOrganizationMessageResponseMessage: action.message,
+      };
     }
     case actions.UPDATEORGANIZTIONMEETING_SUCCESS: {
       return {
         ...state,
         Loading: false,
         UpdateOrganizationMeetingResponse: null,
-        UpdateOrganizationMessageResponseMessage: action.message
-      }
+        UpdateOrganizationMessageResponseMessage: action.message,
+      };
     }
     // action case for adming meeting delete
     case actions.DELETEORGANIZATIONMEETING_INIT: {
       return {
         ...state,
-        Loading: true
-      }
+        Loading: true,
+      };
     }
     case actions.DELETEORGANIZATIONMEETING_SUCCESS: {
       return {
         ...state,
         Loading: false,
         DeleteOrganizationMeetingResponse: action.response,
-        DeleteOrganizationMessageResponseMessage: action.message
-      }
+        DeleteOrganizationMessageResponseMessage: action.message,
+      };
     }
     case actions.DELETEORGANIZATIONMEETING_FAIL: {
       return {
         ...state,
         Loading: false,
         DeleteOrganizationMeetingResponse: null,
-        DeleteOrganizationMessageResponseMessage: action.message
-      }
+        DeleteOrganizationMessageResponseMessage: action.message,
+      };
     }
     //action Case For Admin-Packagedetail
     case actions.ADMIN_PACKAGEDETAIL_INIT:
-      return {}
+      return {};
 
     case actions.ADMIN_PACKAGEDETAIL_SUCCESS:
       return {};
@@ -266,7 +266,6 @@ const adminReducer = (state = initialState, action) => {
         ...state,
         Loading: true,
       };
-
 
     case actions.ADMIN_USERLISTSTATIST_SUCCESS:
       return {
@@ -342,6 +341,25 @@ const adminReducer = (state = initialState, action) => {
         Loading: false,
         ResponseMessage: action.message,
       };
+    case actions.ADMIN_MEETINGSTATUS_INIT:
+      return {
+        ...state,
+        Loading: true,
+      };
+    case actions.ADMIN_MEETINGSTATUS_SUCCESS:
+      return {
+        ...state,
+        Loading: false,
+        ResponseMessage: action.message,
+        AllMeetingsStatus: action.response,
+      };
+    case actions.ADMIN_MEETINGSTATUS_FAIL:
+      return {
+        ...state,
+        Loading: false,
+        ResponseMessage: action.message,
+      };
+
     default:
       return { ...state };
   }
