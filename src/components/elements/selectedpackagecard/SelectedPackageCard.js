2a0caa4fb9345cbb9c65ba55cccc405a232afe8c
Owais Wajid Khan <43402102+OwaisKhan17@users.noreply.github.com>
2023-02-22 20:13:12 +0500

    Admin Issues Day 2

--- src/components/elements/selectedpackagecard/SelectedPackageCard.js
+++ src/components/elements/selectedpackagecard/SelectedPackageCard.js
@@ -1,26 +1,39 @@
-import React, { useState } from 'react'
-import styles from './SelectPackageCard.module.css'
-import { Container, Row, Col } from 'react-bootstrap'
-const SelectedPackageCard = ({PackageHeading, RowsData, PackageHeadingClass}) => {
-    const [rows, setRows ] = useState([])
-    return (
-        <Row>
-            <Col className={styles["payoutstandingBox"]} sm={12} md={12} lg={12} >
-                <Container className={styles["selected_package_Container"]}>
-                    <Row>
-                        <Col sm={12} md={12} lg={12} className={PackageHeadingClass}>
-                            {PackageHeading}
-                        </Col>
-                    </Row>
-                    {RowsData}
-            
-             
-            
+import React, { useState } from "react";
+import styles from "./SelectPackageCard.module.css";
+import { Container, Row, Col } from "react-bootstrap";
+const SelectedPackageCard = ({
+  PackageHeading,
+  RowsData,
+  PackageHeadingClass,
+}) => {
+  const [rows, setRows] = useState([]);
+  // const [activateBlur, setActivateBlur] = useState(false);
+  // let Blur = localStorage.getItem("blur");
+  // useEffect(() => {
+  //     if (Blur != undefined) {
+  //       console.log("Blur", Blur);
 
-                </Container>
+  //       setActivateBlur(true);
+  //     } else {
+  //       console.log("Blur", Blur);
+
+  //       setActivateBlur(false);
+  //     }
+  //   }, [Blur]);
+  return (
+    <Row>
+      <Col className={styles["payoutstandingBox"]} sm={12} md={12} lg={12}>
+        <Container className={styles["selected_package_Container"]}>
+          <Row>
+            <Col sm={12} md={12} lg={12} className={PackageHeadingClass}>
+              {PackageHeading}
             </Col>
-        </Row>
-    )
-}
+          </Row>
+          {RowsData}
+        </Container>
+      </Col>
+    </Row>
+  );
+};
 
-export default SelectedPackageCard
+export default SelectedPackageCard;
