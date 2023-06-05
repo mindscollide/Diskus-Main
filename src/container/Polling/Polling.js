0f434477ad120cea015487711b925220064dcecb
Ali Raza Mamdani <52799482+alirazamamdani@users.noreply.github.com>
2023-06-05 11:06:16 +0500

    code push

--- src/container/Polling/Polling.js
+++ src/container/Polling/Polling.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import styles from "./Polling.module.css";
 import { Row, Col } from "react-bootstrap";
-import { Button, Table } from "../../components/elements";
+import { Button, Table, TextField } from "../../components/elements";
 import { useTranslation } from "react-i18next";
+import searchicon from "../../assets/images/searchicon.svg";
+import CreatePolling from "./CreatePolling/CreatePolling";
+
+
 
 const Polling = () => {
   const [isCreatePoll, setIsCreatePoll] = useState(false);
@@ -47,9 +51,22 @@ const Polling = () => {
             <span className={styles["Poll_Container__heading"]}>Polls</span>
           </Col>
           <Col sm={12} md={2} lg={2}>
-            <Button text={`+ ${t("New")}`} className={styles["new_Poll_Button"]} />
+            <Button text={`+ ${t("New")}`} className={styles["new_Poll_Button"]} onClick={() => setIsCreatePoll(true)} />
           </Col>
-          <Col sm={12} md={9} lg={9}></Col>
+          <Col sm={12} md={9} lg={9} className="position-relative justify-content-end d-flex">
+            {/* <TextField width={"500px"} placeholder={"Search"} /> */}
+            <TextField
+              // value={filterVal}
+              // change={handleFilter}
+              placeholder={t("Search")}
+              applyClass={"PollingSearchInput"}
+              labelClass="d-none"
+              // onDoubleClick={searchbardropdownShow}
+              inputicon={<img src={searchicon} />}
+              // clickIcon={SearchiconClickOptions}
+              iconClassName={styles["polling_searchinput"]}
+            />
+          </Col>
         </Row>
         <Row>
           <Col sm={12} md={12} lg={12}>
@@ -57,6 +74,9 @@ const Polling = () => {
           </Col>
         </Row>
       </section>
+      {isCreatePoll &&
+        <CreatePolling setShowPollingModal={setIsCreatePoll} showPollingModal={isCreatePoll} />
+      }
     </>
   );
 };
