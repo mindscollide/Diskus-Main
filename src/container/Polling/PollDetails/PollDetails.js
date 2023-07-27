8f39efc8a76e0b270dcb85f05efd95cbd2dee6a2
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2023-07-27 16:05:39 +0500

    pageView Done Vote Details

--- src/container/Polling/PollDetails/PollDetails.js
+++ src/container/Polling/PollDetails/PollDetails.js
@@ -19,44 +19,42 @@ const PollDetails = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { PollsReducer } = useSelector((state) => state);
-  const [yesParticipants, setyesParticipants] = useState([]);
-  const [noParticipants, setNoParticipants] = useState([
-    {
-      id: 1,
-      name: "Saad Fudda No",
-    },
-    {
-      id: 2,
-      name: "Saif ul islam No",
-    },
-    {
-      id: 3,
-      name: "Owais Wajid kha No",
-    },
-    {
-      id: 4,
-      name: "Huzeifa Jahangir No",
-    },
-    {
-      id: 5,
-      name: "Ali mamdani No",
-    },
-    {
-      id: 6,
-      name: "Syed Ali raza No",
-    },
-  ]);
   const { t } = useTranslation();
   const [pollId, setPollId] = useState(0);
   let userID = localStorage.getItem("userID");
+  const [pollTitle, setPollTitle] = useState("");
+  const [pollAttendiesOpptionsVise, setPollAttendiesOpptionsVise] = useState(
+    []
+  );
+  const [votePollDetailsOptions, setVotePollDetailsOptions] = useState([]);
+  const [votepollParticipants, setVotePollParticipants] = useState([]);
 
   useEffect(() => {
     let vieVotePollDetails = PollsReducer.viewVotes;
+    let pollOptions = vieVotePollDetails.pollOptions;
+    let pollAttendies = vieVotePollDetails.pollParticipants;
+    let Options = [];
     console.log("handleClosed", vieVotePollDetails);
 
     if (vieVotePollDetails != undefined && vieVotePollDetails != null) {
       if (Object.keys(vieVotePollDetails).length > 0) {
+        // for poll ID
         setPollId(vieVotePollDetails.pollDetails.pollID);
+
+        // for poll Title
+        setPollTitle(vieVotePollDetails.pollDetails.pollTitle);
+
+        // for options
+        if (Object.keys(pollOptions).length > 0) {
+          pollOptions.map((data, index) => {
+            Options.push(data);
+          });
+          setVotePollDetailsOptions(Options);
+        }
+
+        if (Object.keys(pollAttendies).length > 0) {
+          setPollAttendiesOpptionsVise(pollAttendies);
+        }
       }
     }
   }, [PollsReducer.viewVotes]);
@@ -69,6 +67,7 @@ const PollDetails = () => {
     dispatch(getPollsByPollIdApi(navigate, data, 3, t));
     // await dispatch(setviewpollProgressModal(true));
   };
+
   return (
     <Container>
       <Modal
@@ -124,11 +123,10 @@ const PollDetails = () => {
                         lg={12}
                         md={12}
                         sm={12}
-                        className="d-flex align-items-center"
+                        className="d-flex align-items-center mt-2"
                       >
                         <span className={styles["ViewTitleTOShowOnProgress"]}>
-                          Did you receive the material In a sufficient time for
-                          you to prepare for the board meeting, Including agenda
+                          {pollTitle}
                         </span>
                       </Col>
                     </Row>
@@ -141,111 +139,33 @@ const PollDetails = () => {
                     sm={12}
                     className={styles["Scroller_Options"]}
                   >
-                    <Row className="mt-3">
-                      <Col
-                        lg={12}
-                        md={12}
-                        sm={12}
-                        className="m-0 p-0 d-flex gap-5"
-                      >
-                        <span className={styles["no-Of-Yes"]}>
-                          Option 1 - 20%
-                        </span>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col lg={12} md={12} sm={12} className="m-0 p-0">
-                        <Progress
-                          percent={20}
-                          className="pollsDetailsProgress"
-                          status="active"
-                        />
-                      </Col>
-                    </Row>
-                    <Row className="mt-2">
-                      <Col
-                        lg={12}
-                        md={12}
-                        sm={12}
-                        className="m-0 p-0 d-flex gap-5"
-                      >
-                        <span className={styles["no-Of-Yes"]}>
-                          Option 2 - 30%
-                        </span>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col lg={12} md={12} sm={12} className="m-0 p-0">
-                        <Progress
-                          percent={30}
-                          className="pollsDetailsProgress"
-                          status="active"
-                        />
-                      </Col>
-                    </Row>
-                    <Row className="mt-2">
-                      <Col
-                        lg={12}
-                        md={12}
-                        sm={12}
-                        className="m-0 p-0 d-flex gap-5"
-                      >
-                        <span className={styles["no-Of-Yes"]}>
-                          Option 3 - 10%
-                        </span>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col lg={12} md={12} sm={12} className="m-0 p-0">
-                        <Progress
-                          percent={10}
-                          className="pollsDetailsProgress"
-                          status="active"
-                        />
-                      </Col>
-                    </Row>
-                    <Row className="mt-2">
-                      <Col
-                        lg={12}
-                        md={12}
-                        sm={12}
-                        className="m-0 p-0 d-flex gap-5"
-                      >
-                        <span className={styles["no-Of-Yes"]}>
-                          Option 4 - 20%
-                        </span>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col lg={12} md={12} sm={12} className="m-0 p-0">
-                        <Progress
-                          percent={20}
-                          className="pollsDetailsProgress"
-                          status="active"
-                        />
-                      </Col>
-                    </Row>
-                    <Row className="mt-2">
-                      <Col
-                        lg={12}
-                        md={12}
-                        sm={12}
-                        className="m-0 p-0 d-flex gap-5"
-                      >
-                        <span className={styles["no-Of-Yes"]}>
-                          Option 5 - 20%
-                        </span>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col lg={12} md={12} sm={12} className="m-0 p-0">
-                        <Progress
-                          percent={20}
-                          className="pollsDetailsProgress"
-                          status="active"
-                        />
-                      </Col>
-                    </Row>
+                    {votePollDetailsOptions.map((data, index) => {
+                      return (
+                        <>
+                          <Row className="mt-3">
+                            <Col
+                              lg={12}
+                              md={12}
+                              sm={12}
+                              className="m-0 p-0 d-flex gap-5"
+                            >
+                              <span className={styles["no-Of-Yes"]}>
+                                {data.answer} - {data.votePercentage}%
+                              </span>
+                            </Col>
+                          </Row>
+                          <Row>
+                            <Col lg={12} md={12} sm={12} className="m-0 p-0">
+                              <Progress
+                                percent={data.votePercentage}
+                                className="pollsDetailsProgress"
+                                status="active"
+                              />
+                            </Col>
+                          </Row>
+                        </>
+                      );
+                    })}
                   </Col>
                 </Row>
 
@@ -263,83 +183,71 @@ const PollDetails = () => {
                     sm={12}
                     className={styles["Scroller_participants"]}
                   >
-                    <Row>
-                      <Col lg={12} md={12} sm={12} className="m-0 p-0 mt-2">
-                        <span className={styles["Yes_voters"]}>{t("Yes")}</span>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col lg={12} md={12} sm={12}>
-                        <Row>
-                          {yesParticipants.map((data, index) => {
-                            console.log(data, "datadatadatadata");
-                            return (
-                              <Col lg={6} md={6} sm={12} className="mt-2">
-                                <Row>
-                                  <Col
-                                    lg={11}
-                                    md={11}
-                                    sm={12}
-                                    className="m-0 p-0"
-                                  >
-                                    <Row className={styles["Card_border2"]}>
-                                      <Col sm={12} md={12} lg={12}>
-                                        <img
-                                          src={profile}
-                                          width="33px"
-                                          height="33px"
-                                        />
-                                        <span className={styles["Name_cards"]}>
-                                          {data.userName}
-                                        </span>
-                                      </Col>
-                                    </Row>
-                                  </Col>
-                                </Row>
-                              </Col>
-                            );
-                          })}
-                        </Row>
-                      </Col>
-                    </Row>
-                    <Row className="mt-2">
-                      <Col lg={12} md={12} sm={12} className="m-0 p-0">
-                        <span className={styles["Yes_voters"]}>{t("No")}</span>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col lg={12} md={12} sm={12}>
-                        <Row>
-                          {noParticipants.map((data, index) => {
-                            return (
-                              <Col lg={6} md={6} sm={12} className="mt-2">
-                                <Row>
-                                  <Col
-                                    lg={11}
-                                    md={11}
-                                    sm={12}
-                                    className="m-0 p-0"
-                                  >
-                                    <Row className={styles["Card_border2"]}>
-                                      <Col sm={12} md={12} lg={12}>
-                                        <img
-                                          src={profile}
-                                          width="33px"
-                                          height="33px"
-                                        />
-                                        <span className={styles["Name_cards"]}>
-                                          {data.name}
-                                        </span>
-                                      </Col>
-                                    </Row>
-                                  </Col>
-                                </Row>
-                              </Col>
-                            );
-                          })}
-                        </Row>
-                      </Col>
-                    </Row>
+                    {pollAttendiesOpptionsVise.map((data, index) => {
+                      return (
+                        <>
+                          <Row className="mt-2">
+                            <Col lg={12} md={12} sm={12} className="m-0 p-0">
+                              <span className={styles["Yes_voters"]}>
+                                {data.answer +
+                                  " " +
+                                  "(" +
+                                  data.totalVotes +
+                                  ")"}
+                              </span>
+                            </Col>
+                          </Row>
+                          <Row>
+                            {Object.keys(data.pollParticipants).length > 0
+                              ? data.pollParticipants.map(
+                                  (innerData, index) => {
+                                    return (
+                                      <>
+                                        <Col
+                                          lg={6}
+                                          md={6}
+                                          sm={12}
+                                          className="mt-2"
+                                        >
+                                          <Row>
+                                            <Col
+                                              lg={11}
+                                              md={11}
+                                              sm={12}
+                                              className="m-0 p-0"
+                                            >
+                                              <Row
+                                                className={
+                                                  styles["Card_border2"]
+                                                }
+                                              >
+                                                <Col sm={12} md={12} lg={12}>
+                                                  <img
+                                                    src={profile}
+                                                    width="33px"
+                                                    height="33px"
+                                                  />
+                                                  <span
+                                                    className={
+                                                      styles["Name_cards"]
+                                                    }
+                                                  >
+                                                    {innerData.userName}
+                                                  </span>
+                                                </Col>
+                                              </Row>
+                                            </Col>
+                                          </Row>
+                                        </Col>
+                                      </>
+                                    );
+                                  }
+                                )
+                              : null}
+                          </Row>
+                        </>
+                      );
+                    })}
                   </Col>
                 </Row>
               </Col>
