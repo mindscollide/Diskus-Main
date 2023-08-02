279cbe93430a44000ea5c67a50f4ec70832dd94b
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2023-08-02 19:48:52 +0500

    code push

--- src/container/pages/meeting/MeetingTwo.js
+++ src/container/pages/meeting/MeetingTwo.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./meetingTwo.module.css";
 import searchicon from "../../../assets/images/searchicon.svg";
+import BlackCrossIcon from "../../../assets/images/BlackCrossIconModals.svg";
+import ClipIcon from "../../../assets/images/ClipIcon.png";
+import CommentIcon from "../../../assets/images/Comment-Icon.png";
+import EditIcon from "../../../assets/images/Edit-Icon.png";
 import { useTranslation } from "react-i18next";
+import { Pagination, Tooltip } from "antd";
+import Select from "react-select";
 import {
   Button,
   Table,
@@ -10,9 +16,172 @@ import {
   Notification,
 } from "../../../components/elements";
 import { Col, Row } from "react-bootstrap";
-import { Plus } from "react-bootstrap-icons";
+import { ChevronDown, Plus } from "react-bootstrap-icons";
+import moment from "moment";
 const NewMeeting = () => {
   const { t } = useTranslation();
+  let currentLanguage = localStorage.getItem("i18nextLng");
+  const [searchMeeting, setSearchMeeting] = useState(false);
+  // data for rows for first table
+  const data = [
+    {
+      key: "1",
+      pollTitle: (
+        <label className="column-boldness">
+          Board Member Executive Meeting from Boss's exe...
+        </label>
+      ),
+      status: <label className="column-boldness">Active</label>,
+      Organizer: <label className="column-boldness">Mr. Abdul Qadir</label>,
+      Date: <label className="column-boldness"> 3:30pm - 17th May, 2020</label>,
+    },
+  ];
+  const [rowsData, setRowsData] = useState(data);
+  console.log(rowsData, "rowsDatarowsDatarowsData");
+  const HandleShowSearch = () => {
+    setSearchMeeting(!searchMeeting);
+  };
+  const HandleCloseSearchModalMeeting = () => {
+    setSearchMeeting(false);
+  };
+
+  useEffect(() => {
+    if (currentLanguage === "ar") {
+      moment.locale(currentLanguage);
+    } else if (currentLanguage === "fr") {
+      moment.locale(currentLanguage);
+    } else {
+      moment.locale(currentLanguage);
+    }
+  }, [currentLanguage]);
+  const MeetingColoumns = [
+    {
+      title: (
+        <>
+          <Row>
+            <Col lg={12} md={12} sm={12}>
+              <span>{t("Title")}</span>
+            </Col>
+          </Row>
+        </>
+      ),
+      dataIndex: "pollTitle",
+      key: "pollTitle",
+      width: "185px",
+    },
+    {
+      title: t("Status"),
+      dataIndex: "status",
+      key: "status",
+      width: "40px",
+      filters: [
+        {
+          text: t("Published"),
+          value: "Published",
+          className: currentLanguage,
+        },
+        {
+          text: t("Unpublished"),
+          value: "Unpublished",
+        },
+      ],
+      defaultFilteredValue: ["Published", "Unpublished"],
+      filterIcon: (filtered) => (
+        <ChevronDown className="filter-chevron-icon-todolist" />
+      ),
+    },
+    {
+      title: t("Organizer"),
+      dataIndex: "Organizer",
+      key: "Organizer",
+      width: "60px",
+    },
+    {
+      title: t("Date-time"),
+      dataIndex: "Date",
+      key: "Date",
+      width: "87px",
+    },
+    {
+      dataIndex: "Chat",
+      key: "Chat",
+      width: "33px",
+      render: (text, record) => {
+        return (
+          <>
+            <Row>
+              <Col sm={12} md={12} lg={12} className="d-flex gap-2">
+                <Tooltip placement="topRight" title={t("ClipIcon")}>
+                  <img
+                    src={ClipIcon}
+                    className="cursor-pointer"
+                    width="14.02px"
+                    height="16.03px"
+                  />
+                </Tooltip>
+                <Tooltip placement="topLeft" title={t("Delete")}>
+                  <img
+                    src={CommentIcon}
+                    className="cursor-pointer"
+                    width="20.06px"
+                    height="15.95px"
+                  />
+                </Tooltip>
+              </Col>
+            </Row>
+          </>
+        );
+      },
+    },
+    {
+      dataIndex: "Join",
+      key: "Join",
+      width: "53px",
+      render: (text, record) => {
+        return (
+          <>
+            <Row>
+              <Col sm={12} md={12} lg={12}>
+                <Button
+                  text={t("Join-meeting")}
+                  className={styles["joining-Meeting"]}
+                />
+              </Col>
+            </Row>
+          </>
+        );
+      },
+    },
+    {
+      dataIndex: "Edit",
+      key: "Edit",
+      width: "33px",
+      render: (text, record) => {
+        return (
+          <>
+            <Row>
+              <Col
+                sm={12}
+                md={12}
+                lg={12}
+                className="d-flex justify-content-end"
+              >
+                <Tooltip placement="topRight" title={t("Edit")}>
+                  <img
+                    src={EditIcon}
+                    className="cursor-pointer"
+                    width="17.11px"
+                    height="17.11px"
+                  />
+                </Tooltip>
+              </Col>
+            </Row>
+          </>
+        );
+      },
+    },
+  ];
+
   return (
     <section className={styles["NewMeeting_container"]}>
       <Row className="mt-2">
@@ -37,7 +206,7 @@ const NewMeeting = () => {
               applyClass={"meetingSearch"}
               name={"SearchVal"}
               labelClass="d-none"
-              // clickIcon={HandleShowSearch}
+              clickIcon={HandleShowSearch}
               inputicon={
                 <img
                   src={searchicon}
@@ -46,7 +215,85 @@ const NewMeeting = () => {
               }
               iconClassName={styles["polling_searchinput"]}
             />
+            {searchMeeting ? (
+              <>
+                <Row>
+                  <Col
+                    lg={12}
+                    md={12}
+                    sm={12}
+                    className={styles["Search-Box_meeting"]}
+                  >
+                    <Row className="mt-2">
+                      <Col
+                        lg={12}
+                        md={12}
+                        sm={12}
+                        className="d-flex justify-content-end"
+                      >
+                        <img
+                          src={BlackCrossIcon}
+                          className={styles["Cross_Icon_Styling"]}
+                          width="16px"
+                          height="16px"
+                          onClick={HandleCloseSearchModalMeeting}
+                        />
+                      </Col>
+                    </Row>
+                    <Row className="mt-4">
+                      <Col lg={12} md={12} sm={12}>
+                        <TextField
+                          placeholder={t("Meeting-title")}
+                          applyClass={"meetinInnerSearch"}
+                          labelClass="d-none"
+                        />
+                      </Col>
+                    </Row>
+                    <Row className="mt-3">
+                      <Col lg={6} md={6} sm={12}>
+                        <Select />
+                      </Col>
+                      <Col lg={6} md={6} sm={12}>
+                        <TextField
+                          placeholder={t("Organizer-name")}
+                          labelClass="d-none"
+                          applyClass={"meetinInnerSearch"}
+                        />
+                      </Col>
+                    </Row>
+                    <Row className="mt-4">
+                      <Col
+                        lg={12}
+                        md={12}
+                        sm={12}
+                        className="d-flex justify-content-end gap-2"
+                      >
+                        <Button
+                          text={t("Reset")}
+                          className={styles["ResetButtonMeeting"]}
+                        />
+                        <Button
+                          text={t("Search")}
+                          className={styles["SearchButtonMeetings"]}
+                        />
+                      </Col>
+                    </Row>
+                  </Col>
+                </Row>
+              </>
+            ) : null}
           </span>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={12} md={12} sm={12}>
+          <Table
+            column={MeetingColoumns}
+            scroll={{ y: "62vh" }}
+            pagination={false}
+            className="Polling_table"
+            rows={rowsData}
+          />
         </Col>
       </Row>
     </section>
