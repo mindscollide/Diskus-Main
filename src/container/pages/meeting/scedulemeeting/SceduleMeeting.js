a9ce5e09a550a2255ee89861c22b04b0afb751a9
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2023-08-03 14:49:19 +0500

    options created

--- src/container/pages/meeting/scedulemeeting/SceduleMeeting.js
+++ src/container/pages/meeting/scedulemeeting/SceduleMeeting.js
@@ -1,13 +1,69 @@
 import React from "react";
 import styles from "./SceduleMeeting.module.css";
 import { Col, Row } from "react-bootstrap";
-
+import { useTranslation } from "react-i18next";
+import { Paper } from "@material-ui/core";
+import {
+  Button,
+  TextField,
+  Loader,
+  Notification,
+} from "../../../../components/elements";
 const SceduleMeeting = ({ setSceduleMeeting }) => {
+  const { t } = useTranslation();
   return (
     <section>
+      <Row className="mt-2">
+        <Col lg={12} md={12} sm={12}>
+          <span className={styles["Scedule_newMeeting_Heading"]}>
+            {t("Schedule-new-meeting")}
+          </span>
+        </Col>
+      </Row>
       <Row>
         <Col lg={12} md={12} sm={12}>
-          Hello there I am sceduling a Meeting
+          <Paper className={styles["Scedule_meeting_paper"]}>
+            <Row>
+              <Col lg={12} md={12} sm={12} className="d-flex gap-2">
+                <Button
+                  text={t("Meeting-details")}
+                  className={styles["Schedule_meetings_options"]}
+                />
+                <Button
+                  text={t("Organizers")}
+                  className={styles["Schedule_meetings_options"]}
+                />
+                <Button
+                  text={t("Agenda-contributors")}
+                  className={styles["Schedule_meetings_options"]}
+                />
+                <Button
+                  text={t("Participants")}
+                  className={styles["Schedule_meetings_options"]}
+                />
+                <Button
+                  text={t("Agenda")}
+                  className={styles["Schedule_meetings_options"]}
+                />
+                <Button
+                  text={t("Meeting-material")}
+                  className={styles["Schedule_meetings_options"]}
+                />
+                <Button
+                  text={t("Minutes")}
+                  className={styles["Schedule_meetings_options"]}
+                />
+                <Button
+                  text={t("Actions")}
+                  className={styles["Schedule_meetings_options"]}
+                />
+                <Button
+                  text={t("Polls")}
+                  className={styles["Schedule_meetings_options"]}
+                />
+              </Col>
+            </Row>
+          </Paper>
         </Col>
       </Row>
     </section>
