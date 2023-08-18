22eec9d7b9bc3e21c2f1b011bad77ccab4049458
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2023-08-18 18:53:43 +0500

    side bar of advanced permission

--- src/container/pages/meeting/scedulemeeting/Agenda/AdvancePermissionModal/AdvancePersmissionModal.js
+++ src/container/pages/meeting/scedulemeeting/Agenda/AdvancePermissionModal/AdvancePersmissionModal.js
@@ -7,11 +7,14 @@ import Select from "react-select";
 import { useNavigate } from "react-router-dom";
 import { showAdvancePermissionModal } from "../../../../../../store/actions/NewMeetingActions";
 import { Col, Row } from "react-bootstrap";
+import { Breadcrumb, Layout, Menu, theme } from "antd";
 import Key from "../../../../../../assets/images/KEY.svg";
 const AdvancePersmissionModal = () => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const { SubMenu } = Menu;
+  const { Sider } = Layout;
   const { NewMeetingreducer } = useSelector((state) => state);
   return (
     <section>
@@ -61,6 +64,99 @@ const AdvancePersmissionModal = () => {
             <Row>
               <Col lg={12} md={12} sm={12} className="m-0 p-0">
                 <span className={styles["Bottom_line"]}></span>
+              </Col>
+            </Row>
+            <Row>
+              <Col lg={5} md={5} sm={5}>
+                <Row>
+                  <Col lg={12} md={12} sm={12}>
+                    <Row className="mt-3 mb-3">
+                      <Col
+                        lg={11}
+                        md={11}
+                        sm={12}
+                        className="d-flex justify-content-center"
+                      >
+                        <span className={styles["Agenda_heading_Advance"]}>
+                          {t("Agenda")}
+                        </span>
+                      </Col>
+                      <Col lg={1} md={1} sm={12}></Col>
+                    </Row>
+                    <Layout>
+                      <Sider width={250}>
+                        <Menu
+                          theme="light"
+                          // defaultOpenKeys={[defaultOpenKey]}
+                          // defaultSelectedKeys={[defaultSelectedKey]}
+                          mode="inline"
+                          className="Menu-sidebar-class"
+                        >
+                          <SubMenu
+                            key="sub1"
+                            icon={<i className="icon-user menu-icons"></i>}
+                            title="Introduction"
+                            className="submenu-sidebar-icons"
+                          >
+                            <Menu.Item className="menu-items-sidebar" key="3">
+                              Add a Bank User
+                            </Menu.Item>
+
+                            <Menu.Item className="menu-items-sidebar" key="4">
+                              Pending user requests
+                            </Menu.Item>
+                            <Menu.Item className="menu-items-sidebar" key="5">
+                              Bank User List
+                            </Menu.Item>
+                          </SubMenu>
+                          <SubMenu
+                            key="sub2"
+                            icon={<i className="icon-user menu-icons"></i>}
+                            title="CEO Report"
+                            className="submenu-sidebar-icons"
+                          >
+                            <Menu.Item className="menu-items-sidebar" key="6">
+                              Add a Customer
+                            </Menu.Item>
+                            <Menu.Item className="menu-items-sidebar" key="7">
+                              Customer Users List
+                            </Menu.Item>
+                          </SubMenu>
+                          <SubMenu
+                            key="sub3"
+                            icon={<i className="icon-user menu-icons"></i>}
+                            title="Finance Summary"
+                            className="submenu-sidebar-icons"
+                          >
+                            <Menu.Item className="menu-items-sidebar" key="8">
+                              User Report
+                            </Menu.Item>
+                          </SubMenu>
+                          <SubMenu
+                            key="sub4"
+                            icon={<i className="icon-user menu-icons"></i>}
+                            title="Functional Review"
+                            className="submenu-sidebar-icons"
+                          >
+                            <Menu.Item className="menu-items-sidebar" key="9">
+                              Nature Of Business
+                            </Menu.Item>
+                          </SubMenu>
+                          <SubMenu
+                            key="sub5"
+                            icon={<i className="icon-user menu-icons"></i>}
+                            title="Closing Report"
+                            className="submenu-sidebar-icons"
+                          >
+                            <Menu.Item className="menu-items-sidebar" key="9">
+                              Nature Of Business
+                            </Menu.Item>
+                          </SubMenu>
+                        </Menu>
+                      </Sider>
+                    </Layout>
+                  </Col>
+                </Row>
               </Col>
             </Row>
           </>
