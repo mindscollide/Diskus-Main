c2d697ceee41a940a0236064fae2d1a420117e64
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2023-08-18 12:42:43 +0500

    started new meeting design

--- src/container/pages/meeting/scedulemeeting/Agenda/AdvancePermissionModal/AdvancePersmissionModal.js
+++ src/container/pages/meeting/scedulemeeting/Agenda/AdvancePermissionModal/AdvancePersmissionModal.js
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { showAdvancePermissionModal } from "../../../../../../store/actions/NewMeetingActions";
 import { Col, Row } from "react-bootstrap";
+import Key from "../../../../../../assets/images/KEY.svg";
 const AdvancePersmissionModal = () => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
