3ae67e915b948d708b52c57db2d5c8ba16f74bb2
Owais Wajid Khan <43402102+OwaisKhan17@users.noreply.github.com>
2023-08-29 17:13:36 +0500

    Loader Fixed

--- src/components/elements/loader/Loader.js
+++ src/components/elements/loader/Loader.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Logo from '../../../assets/images/newElements/Diskus_newLogo.svg'
 import { Container, Row, Col } from 'react-bootstrap'
 import styles from './Loader.module.css'
-import { useTranslation } from 'react-i18next'
 import newlogo from '../../../assets/images/Newlogo.svg'
+import { useTranslation } from 'react-i18next'
 
 const Loader = () => {
   const { t } = useTranslation()
@@ -24,18 +24,6 @@ const Loader = () => {
   const [randomIndex, setRandomIndex] = useState(0)
 
   useEffect(() => {
-    // // Get the last displayed index from local storage or start from 0
-    // const lastDisplayedIndex = localStorage.getItem("lastDisplayedIndex") || 0;
-
-    // // Calculate the next index based on the last displayed index
-    // const nextIndex = (parseInt(lastDisplayedIndex) + 1) % messages.length;
-
-    // // Save the next index in local storage
-    // localStorage.setItem("lastDisplayedIndex", nextIndex);
-
-    // // Update the state with the new index
-    // setRandomIndex(nextIndex);
-
     // Generate a random index
     const randomIdx = Math.floor(Math.random() * messages.length)
 
@@ -48,21 +36,20 @@ const Loader = () => {
       <Row className={styles['overlay-box']}>
         <Col className={styles['overlay']}></Col>
         <Col className={styles['overlay-content']}>
-          <img src={Logo} widt={100} height={100} />
-          {localStorage.getItem('deleteContent') && (
-            <p className={styles['deleteOrganizationContent']}>
-              Please wait while we delete your organization relevant data…
-            </p>
-          )}
-
           <img src={newlogo} widt="269.97px" height="259.69px" />
           {localStorage.getItem('deleteContent') && (
             <p className={styles['deleteOrganizationContent']}>
               {t('Please-wait-loader')}
             </p>
           )}
-
           <div className={styles['loader-line']}></div>
+          <Row>
+            <Col lg={12} md={12} sm={12}>
+              <p className={styles['Messeges_Styles']}>
+                {messages[randomIndex]}
+              </p>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Container>
