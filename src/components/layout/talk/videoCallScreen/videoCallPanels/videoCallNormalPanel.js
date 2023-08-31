dba5745c74bdc9da8573afa7a4b7a0d7a08d814a
Owais Wajid Khan <43402102+OwaisKhan17@users.noreply.github.com>
2023-08-31 14:08:36 +0500

    VideoNormalCall

--- src/components/layout/talk/videoCallScreen/videoCallPanels/videoCallNormalPanel.js
+++ src/components/layout/talk/videoCallScreen/videoCallPanels/videoCallNormalPanel.js
@@ -1,143 +1,143 @@
-import React, { useState, useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import { useSelector, useDispatch } from "react-redux";
-import "./videoCallNormalPanel.css";
-import VideoCallNormalHeader from "../videoCallHeader/videoCallNormalHeader";
-import VideoPanelNormalChat from "./videoCallNormalChat";
-import VideoPanelNormalAgenda from "./videoCallNormalAgenda";
-import VideoPanelNormalMinutesMeeting from "./videoCallNormalMinutesMeeting";
+import React, { useState, useEffect } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import { useSelector, useDispatch } from 'react-redux'
+import './videoCallNormalPanel.css'
+import VideoCallNormalHeader from '../videoCallHeader/videoCallNormalHeader'
+import VideoPanelNormalChat from './videoCallNormalChat'
+import VideoPanelNormalAgenda from './videoCallNormalAgenda'
+import VideoPanelNormalMinutesMeeting from './videoCallNormalMinutesMeeting'
 import {
   agendaEnableNormalFlag,
   chatEnableNormalFlag,
   minutesMeetingEnableNormalFlag,
-} from "../../../../../store/actions/VideoFeature_actions";
+} from '../../../../../store/actions/VideoFeature_actions'
 import {
   endIndexUrl,
   extractedUrl,
   generateURLCaller,
   generateURLParticipant,
-} from "../../../../../commen/functions/urlVideoCalls";
-import ChatNonActive from "../../../../../assets/images/newElements/ChatIconNonActive.svg";
-import NoteNonActive from "../../../../../assets/images/newElements/NoteIconNonActive.svg";
-import Note_2NonActive from "../../../../../assets/images/newElements/Note_2NonActive.svg";
-import ActiveChat from "../../../../../assets/images/newElements/ActiveChatIcon.svg";
-import ActiveNote from "../../../../../assets/images/newElements/ActiveNoteIcon.svg";
-import ActiveNote2 from "../../../../../assets/images/newElements/ActiveNote2Icon.svg";
-import Avatar2 from "../../../../../assets/images/newElements/Avatar2.png";
+} from '../../../../../commen/functions/urlVideoCalls'
+import ChatNonActive from '../../../../../assets/images/newElements/ChatIconNonActive.svg'
+import NoteNonActive from '../../../../../assets/images/newElements/NoteIconNonActive.svg'
+import Note_2NonActive from '../../../../../assets/images/newElements/Note_2NonActive.svg'
+import ActiveChat from '../../../../../assets/images/newElements/ActiveChatIcon.svg'
+import ActiveNote from '../../../../../assets/images/newElements/ActiveNoteIcon.svg'
+import ActiveNote2 from '../../../../../assets/images/newElements/ActiveNote2Icon.svg'
+import Avatar2 from '../../../../../assets/images/newElements/Avatar2.png'
 
 const VideoPanelNormal = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   const { videoFeatureReducer, VideoMainReducer } = useSelector(
-    (state) => state
-  );
+    (state) => state,
+  )
 
-  // let currentUserID = Number(localStorage.getItem('userID'))
+  let currentUserID = Number(localStorage.getItem('userID'))
   // let currentUserName = localStorage.getItem('name')
 
-  let callerID = Number(localStorage.getItem("callerID"));
-  let recipentID = Number(localStorage.getItem("recipentID"));
-  let roomID = localStorage.getItem("RoomID");
-  let callerName = localStorage.getItem("callerName");
-  let recipentName = localStorage.getItem("recipentName");
+  let callerID = Number(localStorage.getItem('callerID'))
+  let recipentID = Number(localStorage.getItem('recipentID'))
+  let roomID = localStorage.getItem('RoomID')
+  let callerName = localStorage.getItem('callerName')
+  let recipentName = localStorage.getItem('recipentName')
 
   const [incomingCallerData, setIncomingCallerData] = useState({
     callerID: 0,
-    callerName: "",
-    roomID: "",
-  });
+    callerName: '',
+    roomID: '',
+  })
 
   const [recipentCallerData, setRecipentCallerData] = useState({
     callerID: 0,
-    callerName: "",
-    roomID: "",
-  });
+    callerName: '',
+    roomID: '',
+  })
 
-  const [callerURL, setCallerURL] = useState("");
-  const [participantURL, setParticipantURL] = useState("");
+  const [callerURL, setCallerURL] = useState('')
+  const [participantURL, setParticipantURL] = useState('')
 
-  const [isActiveIcon, setIsActiveIcon] = useState(false);
-  const [isNoteActive, setIsNoteActive] = useState(false);
-  const [isNote2Active, setIsNote2Active] = useState(false);
+  const [isActiveIcon, setIsActiveIcon] = useState(false)
+  const [isNoteActive, setIsNoteActive] = useState(false)
+  const [isNote2Active, setIsNote2Active] = useState(false)
 
   const onClickCloseChatHandler = () => {
     if (isActiveIcon === false) {
-      dispatch(chatEnableNormalFlag(true));
-      setIsActiveIcon(true);
-      dispatch(agendaEnableNormalFlag(false));
-      setIsNoteActive(false);
-      dispatch(minutesMeetingEnableNormalFlag(false));
-      setIsNote2Active(false);
+      dispatch(chatEnableNormalFlag(true))
+      setIsActiveIcon(true)
+      dispatch(agendaEnableNormalFlag(false))
+      setIsNoteActive(false)
+      dispatch(minutesMeetingEnableNormalFlag(false))
+      setIsNote2Active(false)
     } else {
-      dispatch(chatEnableNormalFlag(false));
-      setIsActiveIcon(false);
-      dispatch(agendaEnableNormalFlag(false));
-      setIsNoteActive(false);
-      dispatch(minutesMeetingEnableNormalFlag(false));
-      setIsNote2Active(false);
+      dispatch(chatEnableNormalFlag(false))
+      setIsActiveIcon(false)
+      dispatch(agendaEnableNormalFlag(false))
+      setIsNoteActive(false)
+      dispatch(minutesMeetingEnableNormalFlag(false))
+      setIsNote2Active(false)
     }
-  };
+  }
 
   const onClickNoteIconHandler = () => {
     if (isNoteActive === false) {
-      dispatch(agendaEnableNormalFlag(true));
-      setIsNoteActive(true);
-      dispatch(chatEnableNormalFlag(false));
-      setIsActiveIcon(false);
-      dispatch(minutesMeetingEnableNormalFlag(false));
-      setIsNote2Active(false);
+      dispatch(agendaEnableNormalFlag(true))
+      setIsNoteActive(true)
+      dispatch(chatEnableNormalFlag(false))
+      setIsActiveIcon(false)
+      dispatch(minutesMeetingEnableNormalFlag(false))
+      setIsNote2Active(false)
     } else {
-      dispatch(agendaEnableNormalFlag(false));
-      setIsNoteActive(false);
-      dispatch(chatEnableNormalFlag(false));
-      setIsActiveIcon(false);
-      dispatch(minutesMeetingEnableNormalFlag(false));
-      setIsNote2Active(false);
+      dispatch(agendaEnableNormalFlag(false))
+      setIsNoteActive(false)
+      dispatch(chatEnableNormalFlag(false))
+      setIsActiveIcon(false)
+      dispatch(minutesMeetingEnableNormalFlag(false))
+      setIsNote2Active(false)
     }
-  };
+  }
 
   const onClickMinutesHandler = () => {
     if (isNote2Active === false) {
-      dispatch(agendaEnableNormalFlag(false));
-      setIsNoteActive(false);
-      dispatch(chatEnableNormalFlag(false));
-      setIsActiveIcon(false);
-      dispatch(minutesMeetingEnableNormalFlag(true));
-      setIsNote2Active(true);
+      dispatch(agendaEnableNormalFlag(false))
+      setIsNoteActive(false)
+      dispatch(chatEnableNormalFlag(false))
+      setIsActiveIcon(false)
+      dispatch(minutesMeetingEnableNormalFlag(true))
+      setIsNote2Active(true)
     } else {
-      dispatch(agendaEnableNormalFlag(false));
-      setIsNoteActive(false);
-      dispatch(chatEnableNormalFlag(false));
-      setIsActiveIcon(false);
-      dispatch(minutesMeetingEnableNormalFlag(false));
-      setIsNote2Active(false);
+      dispatch(agendaEnableNormalFlag(false))
+      setIsNoteActive(false)
+      dispatch(chatEnableNormalFlag(false))
+      setIsActiveIcon(false)
+      dispatch(minutesMeetingEnableNormalFlag(false))
+      setIsNote2Active(false)
     }
-  };
+  }
 
   useEffect(() => {
-    let dynamicBaseURLCaller = localStorage.getItem("videoBaseURLCaller");
-    const endIndexBaseURLCaller = endIndexUrl(dynamicBaseURLCaller);
+    let dynamicBaseURLCaller = localStorage.getItem('videoBaseURLCaller')
+    const endIndexBaseURLCaller = endIndexUrl(dynamicBaseURLCaller)
     const extractedBaseURLCaller = extractedUrl(
       dynamicBaseURLCaller,
-      endIndexBaseURLCaller
-    );
+      endIndexBaseURLCaller,
+    )
     let dynamicBaseURLParticipant = localStorage.getItem(
-      "videoBaseURLParticipant"
-    );
-    const endIndexBaseURLParticipant = endIndexUrl(dynamicBaseURLParticipant);
+      'videoBaseURLParticipant',
+    )
+    const endIndexBaseURLParticipant = endIndexUrl(dynamicBaseURLParticipant)
     const extractedBaseURLParticipant = extractedUrl(
       dynamicBaseURLParticipant,
-      endIndexBaseURLParticipant
-    );
-    setCallerURL(generateURLCaller(extractedBaseURLCaller, callerName, roomID));
+      endIndexBaseURLParticipant,
+    )
+    setCallerURL(generateURLCaller(extractedBaseURLCaller, callerName, roomID))
     setParticipantURL(
-      generateURLParticipant(extractedBaseURLParticipant, recipentName, roomID)
-    );
-  }, []);
+      generateURLParticipant(extractedBaseURLParticipant, recipentName, roomID),
+    )
+  }, [])
 
-  console.log("videoFeatureReducer", videoFeatureReducer);
+  console.log('videoFeatureReducer', videoFeatureReducer)
 
-  console.log("CALLER URL THEN PARTICIPANT", callerURL, participantURL);
+  console.log('CALLER URL THEN PARTICIPANT', callerURL, participantURL)
 
   return (
     <>
@@ -152,7 +152,11 @@ const VideoPanelNormal = () => {
                     <Col lg={12} md={12} sm={12}>
                       <div className="normal-avatar">
                         <iframe
-                          src={callerURL}
+                          src={
+                            currentUserID === callerID
+                              ? callerURL
+                              : participantURL
+                          }
                           title="Live Video"
                           width="100%"
                           height="100%"
@@ -234,7 +238,7 @@ const VideoPanelNormal = () => {
         </Col>
       </Row>
     </>
-  );
-};
+  )
+}
 
-export default VideoPanelNormal;
+export default VideoPanelNormal
