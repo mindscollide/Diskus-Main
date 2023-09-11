22b57b56fd6ffba3f6af7b333c9b3649525c9e83
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2023-09-11 16:27:24 +0500

    proposed meeting dates implemented

--- src/container/pages/meeting/scedulemeeting/SceduleMeeting.js
+++ src/container/pages/meeting/scedulemeeting/SceduleMeeting.js
@@ -16,6 +16,7 @@ import Participants from "./Participants/Participants";
 import Agenda from "./Agenda/Agenda";
 import MeetingMaterial from "./MeetingMaterial/MeetingMaterial";
 import Minutes from "./Minutes/Minutes";
+import ProposedMeetingDate from "./Participants/ProposedMeetingDate/ProposedMeetingDate";
 const SceduleMeeting = ({ setProposeMeetingDate }) => {
   const { t } = useTranslation();
   const [meetingDetails, setmeetingDetails] = useState(true);
@@ -25,6 +26,7 @@ const SceduleMeeting = ({ setProposeMeetingDate }) => {
   const [agenda, setAgenda] = useState(false);
   const [meetingMaterial, setMeetingMaterial] = useState(false);
   const [minutes, setMinutes] = useState(false);
+  const [proposedMeetingDates, setProposedMeetingDates] = useState(false);
   const showMeetingDeitals = () => {
     setmeetingDetails(true);
     setorganizers(false);
@@ -93,125 +95,135 @@ const SceduleMeeting = ({ setProposeMeetingDate }) => {
     setAgenda(false);
   };
   return (
-    <section>
-      <Row className="mt-2">
-        <Col lg={12} md={12} sm={12}>
-          {minutes ? (
-            <>
-              <span className={styles["Scedule_newMeeting_Heading"]}>
-                {t("IT-departmental-meeting")}
-              </span>
-            </>
-          ) : (
-            <>
-              <span className={styles["Scedule_newMeeting_Heading"]}>
-                {t("Schedule-new-meeting")}
-              </span>
-            </>
-          )}
-        </Col>
-      </Row>
-      <Row>
-        <Col lg={12} md={12} sm={12}>
-          <Paper className={styles["Scedule_meeting_paper"]}>
-            <Row>
-              <Col lg={12} md={12} sm={12} className="d-flex gap-2">
-                <Button
-                  text={t("Meeting-details")}
-                  className={
-                    meetingDetails === true
-                      ? styles["Schedule_meetings_options_active"]
-                      : styles["Schedule_meetings_options"]
-                  }
-                  onClick={showMeetingDeitals}
-                />
-                <Button
-                  text={t("Organizers")}
-                  className={
-                    organizers === true
-                      ? styles["Schedule_meetings_options_active"]
-                      : styles["Schedule_meetings_options"]
-                  }
-                  onClick={showOrganizers}
-                />
-                <Button
-                  text={t("Agenda-contributors")}
-                  className={
-                    agendaContributors === true
-                      ? styles["Schedule_meetings_options_active"]
-                      : styles["Schedule_meetings_options"]
-                  }
-                  onClick={showAgendaContributers}
-                />
-                <Button
-                  text={t("Participants")}
-                  className={
-                    participants === true
-                      ? styles["Schedule_meetings_options_active"]
-                      : styles["Schedule_meetings_options"]
-                  }
-                  onClick={showParticipants}
-                />
-                <Button
-                  text={t("Agenda")}
-                  className={
-                    agenda === true
-                      ? styles["Schedule_meetings_options_active"]
-                      : styles["Schedule_meetings_options"]
-                  }
-                  onClick={showAgenda}
-                />
-                <Button
-                  text={t("Meeting-material")}
-                  className={styles["Schedule_meetings_options"]}
-                  onClick={showMeetingMaterial}
-                />
-                <Button
-                  text={t("Minutes")}
-                  className={styles["Schedule_meetings_options"]}
-                  onClick={showMinutes}
-                />
-                <Button
-                  text={t("Actions")}
-                  className={styles["Schedule_meetings_options"]}
-                />
-                <Button
-                  text={t("Polls")}
-                  className={styles["Schedule_meetings_options"]}
-                />
+    <>
+      {proposedMeetingDates ? (
+        <ProposedMeetingDate />
+      ) : (
+        <>
+          <section>
+            <Row className="mt-2">
+              <Col lg={12} md={12} sm={12}>
+                {minutes ? (
+                  <>
+                    <span className={styles["Scedule_newMeeting_Heading"]}>
+                      {t("IT-departmental-meeting")}
+                    </span>
+                  </>
+                ) : (
+                  <>
+                    <span className={styles["Scedule_newMeeting_Heading"]}>
+                      {t("Schedule-new-meeting")}
+                    </span>
+                  </>
+                )}
               </Col>
             </Row>
-            {meetingDetails && (
-              <MeetingDetails
-                setorganizers={setorganizers}
-                setmeetingDetails={setmeetingDetails}
-              />
-            )}
-            {organizers && (
-              <Organizers
-                setAgendaContributors={setAgendaContributors}
-                setorganizers={setorganizers}
-              />
-            )}
-            {agendaContributors && (
-              <AgendaContributers
-                setParticipants={setParticipants}
-                setAgendaContributors={setAgendaContributors}
-              />
-            )}
-            {participants && (
-              <Participants
-                setParticipants={setParticipants}
-                setAgenda={setAgenda}
-              />
-            )}
-            {agenda && <Agenda />}
-            {meetingMaterial && <MeetingMaterial />}
-            {minutes && <Minutes setMinutes={setMinutes} />}
-          </Paper>
-        </Col>
-      </Row>
-    </section>
+            <Row>
+              <Col lg={12} md={12} sm={12}>
+                <Paper className={styles["Scedule_meeting_paper"]}>
+                  <Row>
+                    <Col lg={12} md={12} sm={12} className="d-flex gap-2">
+                      <Button
+                        text={t("Meeting-details")}
+                        className={
+                          meetingDetails === true
+                            ? styles["Schedule_meetings_options_active"]
+                            : styles["Schedule_meetings_options"]
+                        }
+                        onClick={showMeetingDeitals}
+                      />
+                      <Button
+                        text={t("Organizers")}
+                        className={
+                          organizers === true
+                            ? styles["Schedule_meetings_options_active"]
+                            : styles["Schedule_meetings_options"]
+                        }
+                        onClick={showOrganizers}
+                      />
+                      <Button
+                        text={t("Agenda-contributors")}
+                        className={
+                          agendaContributors === true
+                            ? styles["Schedule_meetings_options_active"]
+                            : styles["Schedule_meetings_options"]
+                        }
+                        onClick={showAgendaContributers}
+                      />
+                      <Button
+                        text={t("Participants")}
+                        className={
+                          participants === true
+                            ? styles["Schedule_meetings_options_active"]
+                            : styles["Schedule_meetings_options"]
+                        }
+                        onClick={showParticipants}
+                      />
+                      <Button
+                        text={t("Agenda")}
+                        className={
+                          agenda === true
+                            ? styles["Schedule_meetings_options_active"]
+                            : styles["Schedule_meetings_options"]
+                        }
+                        onClick={showAgenda}
+                      />
+                      <Button
+                        text={t("Meeting-material")}
+                        className={styles["Schedule_meetings_options"]}
+                        onClick={showMeetingMaterial}
+                      />
+                      <Button
+                        text={t("Minutes")}
+                        className={styles["Schedule_meetings_options"]}
+                        onClick={showMinutes}
+                      />
+                      <Button
+                        text={t("Actions")}
+                        className={styles["Schedule_meetings_options"]}
+                      />
+                      <Button
+                        text={t("Polls")}
+                        className={styles["Schedule_meetings_options"]}
+                      />
+                    </Col>
+                  </Row>
+                  {meetingDetails && (
+                    <MeetingDetails
+                      setorganizers={setorganizers}
+                      setmeetingDetails={setmeetingDetails}
+                    />
+                  )}
+                  {organizers && (
+                    <Organizers
+                      setAgendaContributors={setAgendaContributors}
+                      setorganizers={setorganizers}
+                    />
+                  )}
+                  {agendaContributors && (
+                    <AgendaContributers
+                      setParticipants={setParticipants}
+                      setAgendaContributors={setAgendaContributors}
+                    />
+                  )}
+                  {participants && (
+                    <Participants
+                      setParticipants={setParticipants}
+                      setAgenda={setAgenda}
+                      setProposedMeetingDates={setProposedMeetingDates}
+                    />
+                  )}
+
+                  {agenda && <Agenda />}
+                  {meetingMaterial && <MeetingMaterial />}
+                  {minutes && <Minutes setMinutes={setMinutes} />}
+                </Paper>
+              </Col>
+            </Row>
+          </section>
+        </>
+      )}
+    </>
   );
 };
 
