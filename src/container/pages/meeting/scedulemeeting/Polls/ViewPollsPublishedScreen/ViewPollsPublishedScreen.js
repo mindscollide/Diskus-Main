371240ef8c6bb1547ad8dc18e38ef57cd596eea7
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2023-09-13 12:59:36 +0500

    ViewVotesPublishedComplted

--- src/container/pages/meeting/scedulemeeting/Polls/ViewPollsPublishedScreen/ViewPollsPublishedScreen.js
+++ src/container/pages/meeting/scedulemeeting/Polls/ViewPollsPublishedScreen/ViewPollsPublishedScreen.js
@@ -1,7 +1,203 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ViewPollsPublishedScreen.module.css";
+import { useTranslation } from "react-i18next";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Col, Row } from "react-bootstrap";
+import Profile from "../../../../../../assets/images/newprofile.png";
+import { Button, Checkbox } from "../../../../../../components/elements";
+import { Progress } from "antd";
+
 const ViewPollsPublishedScreen = () => {
-  return <div>ViewPollsPublished</div>;
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { NewMeetingreducer } = useSelector((state) => state);
+  const [messeges, setMesseges] = useState([
+    {
+      text: "In-person meetings",
+    },
+    {
+      text: "Video conferences",
+    },
+    {
+      text: "Email communication",
+    },
+  ]);
+  const [participant, setparticipant] = useState([
+    {
+      name: "Saif Ul Islam",
+    },
+    {
+      name: "Saif Ul Islam",
+    },
+    {
+      name: "Saif Ul Islam",
+    },
+    {
+      name: "Saif Ul Islam",
+    },
+  ]);
+  return (
+    <section>
+      <Row>
+        <Col lg={6} md={6} sm={6}>
+          <Row className="mt-3">
+            <Col lg={12} md={12} sm={12}>
+              <span className={styles["Heading_vewPolls_Published"]}>
+                How do you prefer to collaborate with your colleagues in the
+                office?
+              </span>
+            </Col>
+          </Row>
+          <Row>
+            <Col
+              lg={12}
+              md={12}
+              sm={12}
+              className={styles["Scroller_View_Published_Polls"]}
+            >
+              <Row>
+                {messeges.length > 0
+                  ? messeges.map((data, index) => {
+                      return (
+                        <>
+                          <Col lg={12} md={12} sm={12} className="mt-2">
+                            <section>
+                              <Row>
+                                <Col lg={12} md={12} sm={12}>
+                                  <span
+                                    className={styles["Messege_span_Class"]}
+                                  >
+                                    {data.text} <span>(12)</span>
+                                  </span>
+                                </Col>
+                              </Row>
+                              <Row>
+                                <Col lg={12} md={12} sm={12}>
+                                  <Row>
+                                    <Col
+                                      lg={12}
+                                      md={12}
+                                      sm={12}
+                                      className="d-flex gap-3"
+                                    >
+                                      <Checkbox />
+                                      <Progress
+                                        className="Progress_bar_Polls"
+                                        percent={20}
+                                        status="active"
+                                      />
+                                    </Col>
+                                  </Row>
+                                </Col>
+                              </Row>
+                            </section>
+                          </Col>
+                        </>
+                      );
+                    })
+                  : null}
+              </Row>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={6} md={6} sm={6}>
+              <Row>
+                <Col lg={12} md={12} sm={12}>
+                  <span className={styles["Date_Heading"]}>
+                    {t("Due-date")}
+                  </span>
+                </Col>
+              </Row>
+              <Row>
+                <Col lg={12} md={12} sm={12}>
+                  <span className={styles["Date_Fetched"]}>18 March 2023</span>
+                </Col>
+              </Row>
+            </Col>
+            <Col
+              lg={6}
+              md={6}
+              sm={6}
+              className="d-flex justify-content-end align-items-center"
+            >
+              <span className={styles["Multiple_Answers_zstyles"]}>
+                {t("Multiple-answers-allowed")}
+              </span>
+            </Col>
+          </Row>
+        </Col>
+        <Col lg={1} md={1} sm={1}></Col>
+        <Col lg={5} md={5} sm={5}>
+          <Row className="mt-3">
+            <Col lg={12} md={12} sm={12}>
+              <span className={styles["Participants"]}>{"Participant"}</span>
+            </Col>
+          </Row>
+          <Row>
+            <Col
+              lg={12}
+              md={12}
+              sm={12}
+              className={styles["Scroller_View_Published_Polls"]}
+            >
+              <Row>
+                {participant.length > 0
+                  ? participant.map((data, index) => {
+                      return (
+                        <>
+                          <Col lg={6} md={6} sm={6} className="mt-2">
+                            <section className={styles["Partipants_box"]}>
+                              <Row>
+                                <Col
+                                  lg={12}
+                                  md={12}
+                                  sm={12}
+                                  className="d-flex align-items-center gap-2"
+                                >
+                                  <img
+                                    src={Profile}
+                                    height="33px"
+                                    width="33px"
+                                    className={styles["Profile_Style"]}
+                                  />
+                                  <span className={styles["Participants_name"]}>
+                                    {data.name}
+                                  </span>
+                                </Col>
+                              </Row>
+                            </section>
+                          </Col>
+                        </>
+                      );
+                    })
+                  : null}
+              </Row>
+            </Col>
+          </Row>
+          <Row className="mt-4">
+            <Col
+              lg={12}
+              md={12}
+              sm={12}
+              className="d-flex justify-content-end gap-2"
+            >
+              <Button
+                text={t("Cancel")}
+                className={styles["Close_button_View"]}
+              />
+              <Button
+                text={t("View-votes")}
+                className={styles["ViewVotes_button_View"]}
+              />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </section>
+  );
 };
 
 export default ViewPollsPublishedScreen;
