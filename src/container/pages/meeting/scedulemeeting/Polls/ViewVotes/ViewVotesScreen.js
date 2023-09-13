26d9939bdf77194edcec1dfd9bf43b669020e7f4
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2023-09-13 13:06:47 +0500

    ViewVotes SetUp

---
+++ src/container/pages/meeting/scedulemeeting/Polls/ViewVotes/ViewVotesScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./ViewVoteScreen.module.css";
+const ViewVotesScreen = () => {
+  return <div>ViewVotesScreen</div>;
+};
+
+export default ViewVotesScreen;
