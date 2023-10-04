f387b44cce2e15211837449f89a2aa3915cfcfac
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2023-10-04 11:16:29 +0500

    code push approach change

--- src/container/pages/meeting/scedulemeeting/meetingDetails/UnpublishedProposedMeeting/SceduleProposedMeeting/SceduleProposedmeeting.js
+++ src/container/pages/meeting/scedulemeeting/meetingDetails/UnpublishedProposedMeeting/SceduleProposedMeeting/SceduleProposedmeeting.js
@@ -66,6 +66,48 @@ const SceduleProposedmeeting = () => {
     },
   ]);
 
+  const data = [
+    {
+      key: "1",
+      Title: (
+        <>
+          <span className={styles["WidthOFSpan"]}>
+            <span className={styles["ParticipantName"]}>Mr Abdul Qadir</span>
+            <span className={styles["Designation"]}>CFO</span>
+          </span>
+        </>
+      ),
+      Tick: (
+        <>
+          <img src={BlueTick} width="20.7px" height="14.21px" />
+        </>
+      ),
+    },
+  ];
+  const [tablerowsData, setTablerowsData] = useState(data);
+  const MeetingColoumns = [
+    {
+      dataIndex: "Title",
+      key: "Title",
+      width: "215px",
+    },
+    {
+      title: (
+        <>
+          <Row>
+            <Col lg={2} md={2} sm={2}></Col>
+            <Col lg={10} md={10} sm={10}>
+              <span className={styles["DateObject"]}>22-2-2023</span>
+            </Col>
+          </Row>
+        </>
+      ),
+      dataIndex: "Tick",
+      key: "Tick",
+      width: "215px",
+    },
+  ];
+
   return (
     <section>
       <Modal
@@ -90,194 +132,17 @@ const SceduleProposedmeeting = () => {
         }
         ModalBody={
           <>
-            {sceduleProposedmeetingData.length > 0
-              ? sceduleProposedmeetingData.map((data, index) => {
-                  return (
-                    <>
-                      <section className={styles["OverAll_Padding"]}>
-                        <Row>
-                          <Col lg={12} md={12} sm={12}>
-                            {/* Dates Mapping */}
-                            <Row>
-                              <>
-                                <Col lg={3} md={3} sm={3}></Col>
-                                <Col
-                                  lg={9}
-                                  md={9}
-                                  sm={9}
-                                  className="d-flex gap-5"
-                                >
-                                  {data.Dates.length > 0
-                                    ? data.Dates.map((dateData, dateIndex) => {
-                                        return (
-                                          <span
-                                            className={styles["DateObject"]}
-                                          >
-                                            {dateData.dataFormeeting}
-                                          </span>
-                                        );
-                                      })
-                                    : null}
-                                </Col>
-                              </>
-                            </Row>
-                            {/* Members Mapping */}
-                            <section className={styles["InnerSecton"]}>
-                              <Row className="mt-3">
-                                <Col
-                                  lg={12}
-                                  md={12}
-                                  sm={12}
-                                  className={styles["FixedHeight"]}
-                                >
-                                  <Row className="mt-3">
-                                    {data.members.length > 0
-                                      ? data.members.map(
-                                          (membersData, membersIndex) => {
-                                            return (
-                                              <>
-                                                <Row className="mt-2">
-                                                  <Col
-                                                    lg={3}
-                                                    md={3}
-                                                    sm={3}
-                                                    className="mt-1"
-                                                  >
-                                                    <span
-                                                      className={
-                                                        styles["WidthOFSpan"]
-                                                      }
-                                                    >
-                                                      <span
-                                                        className={
-                                                          styles[
-                                                            "ParticipantName"
-                                                          ]
-                                                        }
-                                                      >
-                                                        {membersData.name}
-                                                      </span>
-                                                      <span
-                                                        className={
-                                                          styles["Designation"]
-                                                        }
-                                                      >
-                                                        {
-                                                          membersData.designation
-                                                        }
-                                                      </span>
-                                                    </span>
-                                                  </Col>
-
-                                                  <Col
-                                                    lg={9}
-                                                    md={9}
-                                                    sm={9}
-                                                    className="d-flex gap-5"
-                                                  >
-                                                    <span
-                                                      className={
-                                                        styles["forTick"]
-                                                      }
-                                                    >
-                                                      <img
-                                                        src={BlueTick}
-                                                        width="20.7px"
-                                                        height="14.21px"
-                                                      />
-                                                    </span>
-                                                  </Col>
-                                                </Row>
-                                                {membersIndex <
-                                                  data.members.length - 1 && ( // Check if it's not the last item
-                                                  <Row className="mt-2">
-                                                    <Col
-                                                      lg={12}
-                                                      md={12}
-                                                      sm={12}
-                                                    >
-                                                      <span
-                                                        className={
-                                                          styles["bottom_line"]
-                                                        }
-                                                      ></span>
-                                                    </Col>
-                                                  </Row>
-                                                )}
-                                              </>
-                                            );
-                                          }
-                                        )
-                                      : null}
-                                  </Row>
-                                </Col>
-                              </Row>
-
-                              <Row className="mt-4">
-                                <Col lg={12} md={12} sm={12}>
-                                  <span
-                                    className={styles["UpperTopLine"]}
-                                  ></span>
-                                </Col>
-                              </Row>
-                              {/* Total Mapping */}
-                              <Row className="m-0 p-0">
-                                <Col
-                                  lg={12}
-                                  md={12}
-                                  sm={12}
-                                  className={styles["Sizing"]}
-                                >
-                                  <Row>
-                                    <Col lg={3} md={3} sm={3}>
-                                      <span
-                                        className={styles["TotalCount_HEading"]}
-                                      >
-                                        {t("Total")}
-                                      </span>
-                                    </Col>
-                                    <Col
-                                      lg={9}
-                                      md={9}
-                                      sm={9}
-                                      className="d-flex gap-5"
-                                    >
-                                      {data.Votes.length > 0
-                                        ? data.Votes.map(
-                                            (voteData, voteIndex) => {
-                                              return (
-                                                <>
-                                                  <span
-                                                    className={
-                                                      styles["TotalCount"]
-                                                    }
-                                                  >
-                                                    {voteData.amount}
-                                                  </span>
-                                                </>
-                                              );
-                                            }
-                                          )
-                                        : null}
-                                    </Col>
-                                  </Row>
-                                </Col>
-                              </Row>
-                              <Row>
-                                <Col lg={12} md={12} sm={12}>
-                                  <span
-                                    className={styles["UpperTopLine"]}
-                                  ></span>
-                                </Col>
-                              </Row>
-                            </section>
-                          </Col>
-                        </Row>
-                      </section>
-                    </>
-                  );
-                })
-              : null}
+            <Row>
+              <Col lg={12} md={12} sm={12}>
+                <Table
+                  column={MeetingColoumns}
+                  scroll={{ y: "62vh" }}
+                  pagination={false}
+                  className="Polling_table"
+                  rows={tablerowsData}
+                />
+              </Col>
+            </Row>
           </>
         }
       />
