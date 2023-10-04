646b26db585a360d86d53cf1e77c48f8a88a4b3c
Huzeifa Jahangir <84620132+huzeifajaha@users.noreply.github.com>
2023-10-04 10:32:18 +0500

    changes

--- src/container/pages/meeting/scedulemeeting/Agenda/SubDocumnets.js
+++ src/container/pages/meeting/scedulemeeting/Agenda/SubDocumnets.js
@@ -9,60 +9,65 @@ const SubDocumnets = ({ subAgendaData, parentId }) => {
     <Row>
       <Col lg={12} md={12} sm={12} className={styles["SubAgendaDocScroller"]}>
         <Row>
-          {subAgendaData?.Subfiles?.length > 0
-            ? subAgendaData?.Subfiles?.map(
-                (subAgendaFiles, subAgendaFilesIndex) => {
-                  return (
-                    <>
-                      <Draggable
-                        key={subAgendaFiles.FileID}
-                        draggableId={`parent-attachments-${parentId}-subAgendaID-${subAgendaData.SubAgendaID}-attachments-${subAgendaFiles.FileID}`}
-                        index={subAgendaFilesIndex}
-                      >
-                        {(provided) => (
-                          <div
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                            ref={provided.innerRef}
-                          >
-                            <Col
-                              key={subAgendaFilesIndex}
-                              lg={3}
-                              md={3}
-                              sm={3}
-                              className="mt-2"
+          <div className="d-flex gap-2 flex-wrap  mt-2">
+            {subAgendaData?.Subfiles?.length > 0
+              ? subAgendaData?.Subfiles?.map(
+                  (subAgendaFiles, subAgendaFilesIndex) => {
+                    return (
+                      <>
+                        <Draggable
+                          key={subAgendaFiles.FileID}
+                          draggableId={`parent-attachments-${parentId}-subAgendaID-${subAgendaData.SubAgendaID}-attachments-${subAgendaFiles.FileID}`}
+                          index={subAgendaFilesIndex}
+                        >
+                          {(provided) => (
+                            <div
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              ref={provided.innerRef}
                             >
-                              <section className={styles["cardSubAgenda"]}>
-                                <Row key={subAgendaFilesIndex} className="mt-2">
-                                  <Col
-                                    lg={12}
-                                    md={12}
-                                    sm={12}
-                                    className="d-flex gap-2 align-items-center"
+                              <Col
+                                key={subAgendaFilesIndex}
+                                lg={3}
+                                md={3}
+                                sm={3}
+                                className="mt-2"
+                              >
+                                <section className={styles["cardSubAgenda"]}>
+                                  <Row
+                                    key={subAgendaFilesIndex}
+                                    className="mt-2"
                                   >
-                                    <img
-                                      draggable={false}
-                                      src={PdfIcon}
-                                      height="25.57px"
-                                      width="25.57px"
-                                    />
-                                    <span
-                                      className={styles["SubagendaFilesName"]}
+                                    <Col
+                                      lg={12}
+                                      md={12}
+                                      sm={12}
+                                      className="d-flex gap-2 align-items-center"
                                     >
-                                      {subAgendaFiles.name}
-                                    </span>
-                                  </Col>
-                                </Row>
-                              </section>
-                            </Col>
-                          </div>
-                        )}
-                      </Draggable>
-                    </>
-                  );
-                }
-              )
-            : null}
+                                      <img
+                                        draggable={false}
+                                        src={PdfIcon}
+                                        height="25.57px"
+                                        width="25.57px"
+                                      />
+                                      <span
+                                        className={styles["SubagendaFilesName"]}
+                                      >
+                                        {subAgendaFiles.name}
+                                      </span>
+                                    </Col>
+                                  </Row>
+                                </section>
+                              </Col>
+                            </div>
+                          )}
+                        </Draggable>
+                      </>
+                    );
+                  }
+                )
+              : null}
+          </div>
           <Col lg={12} md={12} sm={12}></Col>
         </Row>
       </Col>
