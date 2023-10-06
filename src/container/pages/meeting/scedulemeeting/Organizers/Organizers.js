c9a2942b89d51e00cb5fdec703f9060733e903a1
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2023-10-06 13:33:38 +0500

    First point all the tables are merged into one

--- src/container/pages/meeting/scedulemeeting/Organizers/Organizers.js
+++ src/container/pages/meeting/scedulemeeting/Organizers/Organizers.js
@@ -33,9 +33,7 @@ const Organizers = ({ setAgendaContributors, setorganizers }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   let currentLanguage = localStorage.getItem("i18nextLng");
-  const [notifcationTable, setNotifcationTable] = useState(false);
   const [viewOrganizers, setviewOrganizers] = useState(false);
-  const [rspvtable, setrspvtable] = useState(false);
   const { NewMeetingreducer } = useSelector((state) => state);
   const openCrossIconModal = () => {
     dispatch(showCrossConfirmationModal(true));
@@ -58,6 +56,32 @@ const Organizers = ({ setAgendaContributors, setorganizers }) => {
         <label className="column-boldness">Saifiiyousuf4002@gmail.com</label>
       ),
       OrganizerTitle: <label className="column-boldness">Organizer</label>,
+      rspv: (
+        <>
+          <img
+            draggable={false}
+            src={rspvGreenIcon}
+            height="30px"
+            width="30px"
+          />
+          {/* <img draggable = {false} src={rspvAbstainIcon} height="30px" width="30px" /> */}
+        </>
+      ),
+      Notification: (
+        <>
+          <Row>
+            <Col lg={7} md={7} sm={7} className="d-flex justify-content-center">
+              <img
+                draggable={false}
+                src={greenMailIcon}
+                height="17.64px"
+                width="12.4px"
+              />
+              {/* <img draggable = {false} src={redMailIcon} height="17.64px" width="12.4px" /> */}
+            </Col>
+          </Row>
+        </>
+      ),
       Primary: <label className="column-boldness">Primary</label>,
     },
     {
@@ -148,200 +172,7 @@ const Organizers = ({ setAgendaContributors, setorganizers }) => {
       dataIndex: "Primary",
       key: "Primary",
       width: "200px",
-      //   render: (text, record) => {
-      //     return (
-      //       <>
-      //         <Row>
-      //           <Col
-      //             sm={12}
-      //             md={12}
-      //             lg={12}
-      //             className="d-flex justify-content-end"
-      //           >
-      //             <Tooltip placement="topRight" title={t("Edit")}>
-      //               <img draggable = {false}
-      //                 // src={EditIcon}
-      //                 className="cursor-pointer"
-      //                 width="17.11px"
-      //                 height="17.11px"
-      //               />
-      //             </Tooltip>
-      //           </Col>
-      //         </Row>
-      //       </>
-      //     );
-      //   },
     },
-
-    {
-      dataIndex: "Close",
-      key: "Close",
-      width: "200px",
-    },
-  ];
-
-  const notificationData = [
-    {
-      key: "1",
-      Name: (
-        <label
-          className={styles["Title_desc"]}
-          onClick={openNotifyOrganizorModal}
-        >
-          Muahmmad Saif
-        </label>
-      ),
-      Email: (
-        <label className="column-boldness">Saifiiyousuf4002@gmail.com</label>
-      ),
-      OrganizerTitle: <label className="column-boldness">Organizer</label>,
-      Notification: (
-        <>
-          <Row>
-            <Col lg={7} md={7} sm={7} className="d-flex justify-content-center">
-              <img
-                draggable={false}
-                src={greenMailIcon}
-                height="17.64px"
-                width="12.4px"
-              />
-              {/* <img draggable = {false} src={redMailIcon} height="17.64px" width="12.4px" /> */}
-            </Col>
-          </Row>
-        </>
-      ),
-      Primary: <label className="column-boldness">Primary</label>,
-    },
-  ];
-
-  const [notificationRows, setnotificationRows] = useState(notificationData);
-
-  const NotifcatoinColoumns = [
-    {
-      title: (
-        <>
-          <Row>
-            <Col lg={12} md={12} sm={12}>
-              <span>{t("Name")}</span>
-            </Col>
-          </Row>
-        </>
-      ),
-      dataIndex: "Name",
-      key: "Name",
-      width: "300px",
-    },
-
-    {
-      title: t("Email"),
-      dataIndex: "Email",
-      key: "Email",
-      width: "400px",
-    },
-    {
-      title: t("Organizer-title"),
-      dataIndex: "OrganizerTitle",
-      key: "OrganizerTitle",
-      width: "300px",
-    },
-
-    {
-      title: t("Notification"),
-      dataIndex: "Notification",
-      key: "Notification",
-      width: "200px",
-    },
-
-    {
-      dataIndex: "Primary",
-      key: "Primary",
-      width: "200px",
-    },
-  ];
-
-  const rspvData = [
-    {
-      key: "1",
-      Name: (
-        <label
-          className={styles["Title_desc"]}
-          onClick={openNotifyOrganizorModal}
-        >
-          Muahmmad Saif
-        </label>
-      ),
-      Email: (
-        <label className="column-boldness">Saifiiyousuf4002@gmail.com</label>
-      ),
-      OrganizerTitle: <label className="column-boldness">Organizer</label>,
-      Notification: (
-        <>
-          <Row>
-            <Col lg={7} md={7} sm={7} className="d-flex justify-content-center">
-              <img
-                draggable={false}
-                src={greenMailIcon}
-                height="17.64px"
-                width="12.4px"
-              />
-              {/* <img draggable = {false} src={redMailIcon} height="17.64px" width="12.4px" /> */}
-            </Col>
-          </Row>
-        </>
-      ),
-      rspv: (
-        <>
-          <img
-            draggable={false}
-            src={rspvGreenIcon}
-            height="30px"
-            width="30px"
-          />
-          {/* <img draggable = {false} src={rspvAbstainIcon} height="30px" width="30px" /> */}
-        </>
-      ),
-      Primary: <label className="column-boldness">Primary</label>,
-    },
-  ];
-
-  const [rspvRows, setRspvRows] = useState(rspvData);
-
-  const rspvColoumns = [
-    {
-      title: (
-        <>
-          <Row>
-            <Col lg={12} md={12} sm={12}>
-              <span>{t("Name")}</span>
-            </Col>
-          </Row>
-        </>
-      ),
-      dataIndex: "Name",
-      key: "Name",
-      width: "300px",
-    },
-
-    {
-      title: t("Email"),
-      dataIndex: "Email",
-      key: "Email",
-      width: "400px",
-    },
-    {
-      title: t("Organizer-title"),
-      dataIndex: "OrganizerTitle",
-      key: "OrganizerTitle",
-      width: "300px",
-    },
-
-    {
-      title: t("Notification"),
-      dataIndex: "Notification",
-      key: "Notification",
-      width: "200px",
-    },
-
     {
       title: t("RSPV"),
       dataIndex: "rspv",
@@ -350,8 +181,14 @@ const Organizers = ({ setAgendaContributors, setorganizers }) => {
     },
 
     {
-      dataIndex: "Primary",
-      key: "Primary",
+      title: t("Notification"),
+      dataIndex: "Notification",
+      key: "Notification",
+      width: "200px",
+    },
+    {
+      dataIndex: "Close",
+      key: "Close",
       width: "200px",
     },
   ];
@@ -363,14 +200,6 @@ const Organizers = ({ setAgendaContributors, setorganizers }) => {
   const handleNextButton = () => {
     setorganizers(false);
     setAgendaContributors(true);
-  };
-
-  const enableNotificationTable = () => {
-    setNotifcationTable(!notifcationTable);
-  };
-
-  const enableRspvTable = () => {
-    setrspvtable(!rspvtable);
   };
 
   const EnableOrganizersView = () => {
@@ -402,7 +231,6 @@ const Organizers = ({ setAgendaContributors, setorganizers }) => {
                       height="12.08px"
                     />
                   }
-                  onClick={enableNotificationTable}
                 />
                 <Button
                   text={t("Edit")}
@@ -415,7 +243,6 @@ const Organizers = ({ setAgendaContributors, setorganizers }) => {
                       height="11.75px"
                     />
                   }
-                  onClick={enableRspvTable}
                 />
                 <Button
                   text={t("Add-more")}
@@ -427,37 +254,13 @@ const Organizers = ({ setAgendaContributors, setorganizers }) => {
             </Row>
             <Row>
               <Col lg={12} md={12} sm={12}>
-                {notifcationTable ? (
-                  <>
-                    <Table
-                      column={NotifcatoinColoumns}
-                      scroll={{ y: "92vh" }}
-                      pagination={false}
-                      className="Polling_table"
-                      rows={notificationRows}
-                    />
-                  </>
-                ) : rspvtable ? (
-                  <>
-                    <Table
-                      column={rspvColoumns}
-                      scroll={{ y: "62vh" }}
-                      pagination={false}
-                      className="Polling_table"
-                      rows={rspvRows}
-                    />
-                  </>
-                ) : (
-                  <>
-                    <Table
-                      column={MeetingColoumns}
-                      scroll={{ y: "62vh" }}
-                      pagination={false}
-                      className="Polling_table"
-                      rows={rowsData}
-                    />
-                  </>
-                )}
+                <Table
+                  column={MeetingColoumns}
+                  scroll={{ y: "62vh" }}
+                  pagination={false}
+                  className="Polling_table"
+                  rows={rowsData}
+                />
               </Col>
             </Row>
           </section>
