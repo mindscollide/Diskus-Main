76bef7e8db38cd39ee9cd02bae060ed8cafa46c2
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2023-11-02 20:25:51 +0500

    User Interface as discussed applied

--- src/container/pages/meeting/scedulemeeting/Minutes/Minutes.js
+++ src/container/pages/meeting/scedulemeeting/Minutes/Minutes.js
@@ -7,7 +7,13 @@ import { Button } from "../../../../../components/elements";
 import { Col, Row } from "react-bootstrap";
 import ReactQuill, { Quill } from "react-quill";
 import { useRef } from "react";
-
+import { Upload } from "antd";
+import featherupload from "../../../../../assets/images/featherupload.svg";
+import Leftploygon from "../../../../../assets/images/Polygon 3.svg";
+import file_image from "../../../../../assets/images/file_image.svg";
+import pdfIcon from "../../../../../assets/images/pdf_icon.svg";
+import CrossIcon from "../../../../../assets/images/CrossIcon.svg";
+import Rightploygon from "../../../../../assets/images/Polygon right.svg";
 // import DrapDropIcon from "../../../../../assets/images/DrapDropIcon.svg";
 // import { message, Upload } from "antd";
 // import download from "../../../../../assets/images/UploaderIcon.svg";
@@ -117,7 +123,13 @@ const Minutes = ({ setMinutes }) => {
   const [fileSize, setFileSize] = useState(0);
   let currentLanguage = localStorage.getItem("i18nextLng");
   const editorRef = useRef(null);
-
+  const { Dragger } = Upload;
+  const [fileForSend, setFileForSend] = useState([]);
+  const [fileAttachments, setFileAttachments] = useState([]);
+  const [open, setOpen] = useState({
+    flag: false,
+    message: "",
+  });
   const navigate = useNavigate();
   const { t } = useTranslation();
   const dispatch = useDispatch();
@@ -184,9 +196,130 @@ const Minutes = ({ setMinutes }) => {
     }
   };
 
+  const props = {
+    name: "file",
+    multiple: true,
+    showUploadList: false,
+    onChange(data) {
+      const { status } = data.file;
+      let fileSizeArr;
+      if (fileAttachments.length > 9) {
+        setOpen({
+          flag: true,
+          message: t("Not-allowed-more-than-10-files"),
+        });
+      } else if (fileAttachments.length > 0) {
+        let flag = false;
+        let sizezero;
+        let size;
+        fileAttachments.map((arData, index) => {
+          if (arData.DisplayAttachmentName === data.file.originFileObj.name) {
+            flag = true;
+          }
+        });
+        if (data.file.size > 10485760) {
+          size = false;
+        } else if (data.file.size === 0) {
+          sizezero = false;
+        }
+        if (size === false) {
+          setTimeout(
+            setOpen({
+              flag: true,
+              message: t("File-size-should-not-be-greater-then-zero"),
+            }),
+            3000
+          );
+        } else if (sizezero === false) {
+          setTimeout(
+            setOpen({
+              flag: true,
+              message: t("File-size-should-not-be-zero"),
+            }),
+            3000
+          );
+        } else if (flag === true) {
+          setTimeout(
+            setOpen({
+              flag: true,
+              message: t("File-already-exists"),
+            }),
+            3000
+          );
+        } else {
+          let file = {
+            DisplayAttachmentName: data.file.name,
+            OriginalAttachmentName: data.file.name,
+            fileSize: data.file.originFileObj.size,
+          };
+          setFileAttachments([...fileAttachments, file]);
+          fileSizeArr = data.file.originFileObj.size + fileSize;
+          setFileForSend([...fileForSend, data.file.originFileObj]);
+          setFileSize(fileSizeArr);
+          // dispatch(FileUploadToDo(navigate, data.file.originFileObj, t));
+        }
+      } else {
+        let sizezero;
+        let size;
+        if (data.file.size > 10485760) {
+          size = false;
+        } else if (data.file.size === 0) {
+          sizezero = false;
+        }
+        if (size === false) {
+          setTimeout(
+            setOpen({
+              flag: true,
+              message: t("File-size-should-not-be-greater-then-zero"),
+            }),
+            3000
+          );
+        } else if (sizezero === false) {
+          setTimeout(
+            setOpen({
+              flag: true,
+              message: t("File-size-should-not-be-zero"),
+            }),
+            3000
+          );
+        } else {
+          let file = {
+            DisplayAttachmentName: data.file.name,
+            OriginalAttachmentName: data.file.name,
+            fileSize: data.file.originFileObj.size,
+          };
+          setFileAttachments([...fileAttachments, file]);
+          fileSizeArr = data.file.originFileObj.size + fileSize;
+          setFileForSend([...fileForSend, data.file.originFileObj]);
+          setFileSize(fileSizeArr);
+        }
+      }
+    },
+    onDrop(e) {},
+    customRequest() {},
+  };
+
+  //Sliders For Attachments
+
+  const SlideLeft = () => {
+    var Slider = document.getElementById("Slider");
+    Slider.scrollLeft = Slider.scrollLeft - 300;
+  };
+
+  const Slideright = () => {
+    var Slider = document.getElementById("Slider");
+    Slider.scrollLeft = Slider.scrollLeft + 300;
+  };
+
+  const handleRemoveFile = (index) => {
+    const updatedFies = [...fileAttachments];
+    updatedFies.splice(index, 1);
+    setFileAttachments(updatedFies);
+  };
+
   return (
     <section>
-      <Row>
+      <Row className="mt-4">
         <Col lg={6} md={6} sm={6}>
           <Row className={styles["Add-note-QuillRow"]}>
             <Col
@@ -211,7 +344,143 @@ const Minutes = ({ setMinutes }) => {
             </Col>
           </Row>
         </Col>
-        <Col lg={6} md={6} sm={6}></Col>
+        <Col lg={6} md={6} sm={6}>
+          <Row className="mt-1">
+            <Col lg={1} md={1} sm={1} className="mt-4">
+              {fileAttachments.length > 2 ? (
+                <>
+                  <Button
+                    icon={
+                      <img
+                        src={Leftploygon}
+                        width="20px"
+                        height="15px"
+                        draggable="false"
+                      />
+                    }
+                    onClick={SlideLeft}
+                    className={styles["Leftpolygon"]}
+                  />
+                </>
+              ) : null}
+            </Col>
+            <Col lg={10} md={10} sm={10}>
+              <Row>
+                <Col
+                  lg={12}
+                  md={12}
+                  sm={12}
+                  className="ScrolllerFiles_Committees"
+                  id="Slider"
+                >
+                  {fileAttachments.length > 0
+                    ? fileAttachments.map((data, index) => {
+                        console.log(data, "datadatadata");
+                        return (
+                          <>
+                            <Col
+                              lg={4}
+                              md={4}
+                              sm={12}
+                              className="position-relative gap-2"
+                            >
+                              <span className={styles["Crossicon_Class"]}>
+                                <img
+                                  src={CrossIcon}
+                                  height="12.68px"
+                                  width="12.68px"
+                                  onClick={() => handleRemoveFile(index)}
+                                />
+                              </span>
+                              <section className={styles["Outer_Box"]}>
+                                <Row>
+                                  <Col lg={12} md={12} sm={12}>
+                                    <img
+                                      src={file_image}
+                                      width={"100%"}
+                                      alt=""
+                                      draggable="false"
+                                    />
+                                  </Col>
+                                </Row>
+
+                                <section
+                                  className={styles["backGround_name_Icon"]}
+                                >
+                                  <Row className="mb-2">
+                                    <Col
+                                      lg={12}
+                                      md={12}
+                                      sm={12}
+                                      className={styles["IconTextClass"]}
+                                    >
+                                      <img
+                                        src={pdfIcon}
+                                        height="10px"
+                                        width="10px"
+                                        className={styles["IconPDF"]}
+                                      />
+                                      <span className={styles["FileName"]}>
+                                        {data.DisplayAttachmentName}
+                                      </span>
+                                    </Col>
+                                  </Row>
+                                </section>
+                              </section>
+                            </Col>
+                          </>
+                        );
+                      })
+                    : null}
+                </Col>
+              </Row>
+            </Col>
+            <Col lg={1} md={1} sm={1} className="mt-4">
+              {fileAttachments.length > 2 ? (
+                <>
+                  <Button
+                    icon={
+                      <img
+                        src={Rightploygon}
+                        width="20px"
+                        height="15px"
+                        draggable="false"
+                      />
+                    }
+                    onClick={Slideright}
+                    className={styles["Leftpolygon"]}
+                  />
+                </>
+              ) : null}
+            </Col>
+          </Row>
+          <Row className="mt-2">
+            <Col lg={12} md={12} sm={12}>
+              <Dragger
+                {...props}
+                className={styles["dragdrop_attachment_create_resolution"]}
+              >
+                <p className="ant-upload-drag-icon">
+                  <span className={styles["create_resolution_dragger"]}>
+                    <img
+                      src={featherupload}
+                      width="18.87px"
+                      height="18.87px"
+                      draggable="false"
+                    />
+                  </span>
+                </p>
+                <p className={styles["ant-upload-text"]}>
+                  {t("Drag-&-drop-or")}
+                  <span className={styles["Choose_file_style"]}>
+                    {t("Choose-file")}
+                  </span>
+                  <span className={styles["here_text"]}>{t("Here")}</span>
+                </p>
+              </Dragger>
+            </Col>
+          </Row>
+        </Col>
       </Row>
     </section>
     // <section>
