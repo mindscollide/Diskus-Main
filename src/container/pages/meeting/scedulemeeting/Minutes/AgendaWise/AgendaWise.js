e783560fda6155189a27f7bf63c0f8ddeae08118
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2023-11-28 00:39:31 +0500

    Scedule Meeting Dates Check Meeting Detiails Are Done

--- src/container/pages/meeting/scedulemeeting/Minutes/AgendaWise/AgendaWise.js
+++ src/container/pages/meeting/scedulemeeting/Minutes/AgendaWise/AgendaWise.js
@@ -1066,6 +1066,7 @@ const AgendaWise = ({ currentMeeting, editorRole }) => {
                                     src={`data:image/jpeg;base64,${data?.userProfilePicture?.displayProfilePictureName}`}
                                     height="39px"
                                     width="39px"
+                                    alt=""
                                     className={styles["Profile_minutes"]}
                                   />
                                 </Col>
