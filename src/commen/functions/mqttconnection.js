dc0401d3a5684dd4d35823c861d341269bfd29e5
Huzeifa Jahangir <84620132+huzeifajaha@users.noreply.github.com>
2023-12-01 16:59:09 +0500

    s

--- src/commen/functions/mqttconnection.js
+++ src/commen/functions/mqttconnection.js
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { setClient } from "../../store/actions/Auth2_actions";
 import Helper from "./history_logout";
 let newClient;
+
 export const onConnected = (newClient, subscribeID) => {
   console.log("Connected to MQTT broker onConnected");
   newClient.subscribe(subscribeID.toString());
@@ -20,26 +21,14 @@ export const mqttConnection = (subscribeID) => {
   console.log("mqtt resquest ");
   var max = 90000;
   var id = min + Math.random() * (max - min);
+
   newClient = new Paho.Client(
     "192.168.18.241",
     8228,
     parseInt(subscribeID) + "-" + id
   );
-  // newClient.connect({
-  //   // cleanSession: false,
-  //   onSuccess: () => {
-  //     console.log("Connected to MQTT broker");
-  //     onConnected(newClient, subscribeID);
-  //   },
-
-  //   onFailure: () => {
-  //     console.log("Connected to MQTT broker onFailedConnect");
-  //     setTimeout(onConnectionLost(subscribeID), 6000);
-  //   },
-  //   keepAliveInterval: 30,
-  //   reconnect: true, // Enable automatic reconnect
-  // });
-  var options = {
+  newClient.connect({
+    // cleanSession: false,
     onSuccess: () => {
       console.log("Connected to MQTT broker");
       onConnected(newClient, subscribeID);
@@ -51,10 +40,7 @@ export const mqttConnection = (subscribeID) => {
     },
     keepAliveInterval: 30,
     reconnect: true, // Enable automatic reconnect
-    userName: process.env.REACT_APP_MQTT_User,
-    password: process.env.REACT_APP_MQTT_Pass,
-  };
-  newClient.connect(options);
+  });
   Helper.socket = newClient;
   setClient(newClient);
 };
