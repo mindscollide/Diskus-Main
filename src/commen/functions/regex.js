2251ca16b5b15cb95d2e17b8dbb31d7607de9c60
Owais Wajid Khan <43402102+OwaisKhan17@users.noreply.github.com>
2023-12-14 13:33:31 +0500

    Regex Commented

--- src/commen/functions/regex.js
+++ src/commen/functions/regex.js
@@ -33,7 +33,7 @@ export const urlPatternValidation = (URL) => {
 
 export const containsStringandNumericCharacters = (value) => {
   // Use the replace method with the regular expression to remove non-alphanumeric characters
-  let cleanedValue = value.replace(/[^a-zA-Z0-9 ]/g, "");
+  let cleanedValue = value.replace(/[^\u0600-\u06FFa-zA-Z0-9 ]/g, "");
 
   // Return the cleaned value
   return cleanedValue;
