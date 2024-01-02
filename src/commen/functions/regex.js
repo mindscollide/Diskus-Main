397857d5f3106821948f50efa4fbfa7bdb159fe0
Ali Raza Mamdani <52799482+alirazamamdani@users.noreply.github.com>
2024-01-02 10:47:18 +0500

    Update regex.js

--- src/commen/functions/regex.js
+++ src/commen/functions/regex.js
@@ -1,6 +1,6 @@
 // its allow only character  space and number and also didnt allow space as a first character
 export const regexOnlyForNumberNCharacters = (data) => {
-  return data.replace(/^\s/, "").replace(/[^a-zA-Z0-9\s]/g, "");
+  return data.replace(/^\s/, "").replace(/[^\u0600-\u06FFa-zA-Z0-9\s]/g, "");
 };
 
 // its allow only character  space and also didnt allow space as a first character
