dd70c3e8ecd1e58755ae927ec1dbb57f57ab3572
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2024-02-19 16:17:40 +0500

    Create todo implemented the multiple file upload

--- src/container/todolistModal/ModalToDoList.js
+++ src/container/todolistModal/ModalToDoList.js
@@ -247,8 +247,6 @@ const ModalToDoList = ({ ModalTitle, setShow, show }) => {
     }
   };
 
-  // Initialize previousFileList to an empty array
-  let previousFileList = [];
   //Upload File Handler
   const uploadFilesToDo = (data) => {
     console.log(data, "uploadFilesToDouploadFilesToDo");
@@ -257,141 +255,72 @@ const ModalToDoList = ({ ModalTitle, setShow, show }) => {
       Object.values(data.target.files),
       "filesArrayfilesArrayfilesArray"
     );
-    // Check if the fileList is the same as the previous one
-    if (JSON.stringify(filesArray) === JSON.stringify(previousFileList)) {
-      return; // Skip processing if it's the same fileList
+
+    let fileSizeArr = fileSize;
+    let flag = false;
+    let sizezero = true;
+    let size = true;
+
+    if (tasksAttachments.TasksAttachments.length > 9) {
+      setOpen({
+        flag: true,
+        message: t("Not-allowed-more-than-10-files"),
+      });
+      return;
     }
-    let fileSizeArr;
-    if (Object.keys(tasksAttachments.TasksAttachments).length === 10) {
-      setTimeout(
-        setOpen({
-          flag: true,
-          message: t("You-can-not-upload-more-then-10-files"),
-        }),
-        3000
-      );
-    } else if (fileSize >= 104857600) {
-      setTimeout(
-        setOpen({
-          open: true,
-          message: t("You-can-not-upload-more-then-100MB-files"),
-        }),
-        3000
-      );
-    } else {
-      const uploadFilePath = data.target.value;
-      const uploadedFile = data.target.files[0];
-      let ext = uploadedFile.name.split(".").pop();
-      let file = tasksAttachments.TasksAttachments;
-      if (
-        ext === "doc" ||
-        ext === "docx" ||
-        ext === "xls" ||
-        ext === "xlsx" ||
-        ext === "pdf" ||
-        ext === "png" ||
-        ext === "txt" ||
-        ext === "jpg" ||
-        ext === "jpeg" ||
-        ext === "gif" ||
-        ext === "csv"
-      ) {
-        let data;
-        let sizezero;
-        let size;
-        if (file.length > 0) {
-          file.map((filename, index) => {
-            if (filename.DisplayAttachmentName === uploadedFile.name) {
-              data = false;
-            }
-          });
-          if (uploadedFile.size > 10485760) {
-            size = false;
-          } else if (uploadedFile.size === 0) {
-            sizezero = false;
-          }
-          if (data === false) {
-            setTimeout(
-              setOpen({
-                flag: true,
-                message: t("File-already-exisit"),
-              }),
-              3000
-            );
-          } else if (size === false) {
-            setTimeout(
-              setOpen({
-                flag: true,
-                message: t("File-size-should-not-be-greater-then-zero"),
-              }),
-              3000
-            );
-          } else if (sizezero === false) {
-            setTimeout(
-              setOpen({
-                flag: true,
-                message: t("File-size-should-not-be-zero"),
-              }),
-              3000
-            );
-          } else {
-            fileSizeArr = uploadedFile.size + fileSize;
-            setFileForSend([...fileForSend, uploadedFile]);
-            setFileSize(fileSizeArr);
-            // dispatch(FileUploadToDo(navigate, uploadedFile, t));
-            file.push({
-              PK_TAID: 0,
-              DisplayAttachmentName: uploadedFile.name,
-              OriginalAttachmentName: uploadFilePath,
-              CreationDateTime: "",
-              FK_TID: 0,
-              fileSize: uploadedFile.size,
-            });
-            setTasksAttachments({ ["TasksAttachments"]: file });
-          }
-        } else {
-          if (uploadedFile.size > 10485760) {
-            size = false;
-          } else if (uploadedFile.size === 0) {
-            sizezero = false;
-          }
-          if (size === false) {
-            setTimeout(
-              setOpen({
-                flag: true,
-                message: t("File-size-should-not-be-greater-then-zero"),
-              }),
-              3000
-            );
-          } else if (sizezero === false) {
-            setTimeout(
-              setOpen({
-                flag: true,
-                message: t("File-size-should-not-be-zero"),
-              }),
-              3000
-            );
-          } else {
-            // dispatch(FileUploadToDo(navigate, uploadedFile, t));
-            fileSizeArr = uploadedFile.size + fileSize;
-            setFileForSend([...fileForSend, uploadedFile]);
-            setFileSize(fileSizeArr);
-            file.push({
-              PK_TAID: 0,
-              DisplayAttachmentName: uploadedFile.name,
-              OriginalAttachmentName: uploadFilePath,
-              CreationDateTime: "",
-              FK_TID: 0,
-              fileSize: uploadedFile.size,
-            });
-            setTasksAttachments({ ["TasksAttachments"]: file });
-          }
-        }
+    filesArray.forEach((fileData, index) => {
+      if (fileData.size > 10485760) {
+        size = false;
+      } else if (fileData.size === 0) {
+        sizezero = false;
       }
-    }
-    // Update previousFileList to current fileList
-    previousFileList = filesArray;
+
+      let fileExists = tasksAttachments.TasksAttachments.some(
+        (oldFileData) => oldFileData.DisplayAttachmentName === fileData.name
+      );
+
+      if (!size) {
+        setTimeout(() => {
+          setOpen({
+            flag: true,
+            message: t("File-size-should-not-be-greater-then-zero"),
+          });
+        }, 3000);
+      } else if (!sizezero) {
+        setTimeout(() => {
+          setOpen({
+            flag: true,
+            message: t("File-size-should-not-be-zero"),
+          });
+        }, 3000);
+      } else if (fileExists) {
+        setTimeout(() => {
+          setOpen({
+            flag: true,
+            message: t("File-already-exists"),
+          });
+        }, 3000);
+      } else {
+        let file = {
+          DisplayAttachmentName: fileData.name,
+          OriginalAttachmentName: fileData.name,
+          fileSize: fileData.size,
+        };
+        setTasksAttachments((prevAttachments) => ({
+          ...prevAttachments,
+          TasksAttachments: [...prevAttachments.TasksAttachments, file],
+        }));
+
+        fileSizeArr += fileData.size;
+        setFileForSend((prevFiles) => [...prevFiles, fileData]);
+        setFileSize(fileSizeArr);
+      }
+      // Update previousFileList to current fileList
+      previousFileList = filesArray;
+    });
   };
+
+  let previousFileList = [];
 
   useEffect(() => {
     // dispatch(GetAllAssigneesToDoList(parseInt(createrID)));
