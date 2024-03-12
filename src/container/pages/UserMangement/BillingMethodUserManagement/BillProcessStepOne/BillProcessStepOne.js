383bf0b2ed0ab27b7654bc58176a4a701bd1dd39
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2024-03-12 12:57:42 +0500

    stepper working on the button now

---
+++ src/container/pages/UserMangement/BillingMethodUserManagement/BillProcessStepOne/BillProcessStepOne.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./BillProcessStepTwo.module.css";
+import { Container } from "react-bootstrap";
+const BillProcessStepOne = () => {
+  return <Container>Hello step no one</Container>;
+};
+
+export default BillProcessStepOne;
--- src/container/pages/UserMangement/BillingMethodUserManagement/BillingMethodUsermanagement.js
+++ src/container/pages/UserMangement/BillingMethodUserManagement/BillingMethodUsermanagement.js
@@ -1,10 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./BillingMethodUserManagement.module.css";
 import { useTranslation } from "react-i18next";
 import { Col, Row } from "react-bootstrap";
 import { Step, Stepper } from "react-form-stepper";
+import { Button } from "../../../../components/elements";
+import BillProcessStepOne from "./BillProcessStepOne/BillProcessStepOne";
 const BillingMethodUsermanagement = () => {
   const { t } = useTranslation();
+
+  const [activeStep, setActiveStep] = useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
   //React Stepper Numbers manuipulation
   useEffect(() => {
     const firstvaue = document.querySelector(".StepButtonContent-d1-0-3-7");
@@ -45,12 +57,47 @@ const BillingMethodUsermanagement = () => {
       </Row>
       <Row>
         <Col lg={12} md={12} sm={12}>
-          <Stepper activeStep={1}>
+          <Stepper activeStep={activeStep}>
             <Step label={t("Billing-contact")} />
             <Step label={t("Billing-address")} />
             <Step label={t("Pakage-details")} />
             <Step label={t("Payment-method")} />
           </Stepper>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={12} md={12} sm={12} xs={12}>
+          {activeStep === 0 ? (
+            <BillProcessStepOne />
+          ) : activeStep === 1 ? (
+            <>step number two</>
+          ) : activeStep === 2 ? (
+            <>step number three</>
+          ) : activeStep === 3 ? (
+            <>
+              <>step number four</>
+            </>
+          ) : null}
+        </Col>
+      </Row>
+      <Row>
+        <Col
+          lg={11}
+          md={11}
+          sm={11}
+          xs={11}
+          className="d-flex justify-content-end gap-2 "
+        >
+          <Button
+            text={t("Next")}
+            className={styles["NextbuttonBillingMethod"]}
+            onClick={handleNext}
+          />
+          <Button
+            text={t("Back")}
+            className={styles["BackbuttonBillingMethod"]}
+            onClick={handleBack}
+          />
         </Col>
       </Row>
     </section>
