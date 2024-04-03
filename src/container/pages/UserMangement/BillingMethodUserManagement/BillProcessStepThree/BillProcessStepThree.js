5f74bb82ce073b53c572e0fa0639388430ec4349
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2024-04-03 12:42:08 +0500

    code push api integrated in the table

--- src/container/pages/UserMangement/BillingMethodUserManagement/BillProcessStepThree/BillProcessStepThree.js
+++ src/container/pages/UserMangement/BillingMethodUserManagement/BillProcessStepThree/BillProcessStepThree.js
@@ -4,7 +4,7 @@ import { Col, Container, Row } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import { Button, TableToDo } from "../../../../../components/elements";
 import ellipses from "../../../../../assets/images/ellipses.svg";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getOrganizationSelectedPakagesAPI } from "../../../../../store/actions/UserManagementActions";
 import { useNavigate } from "react-router-dom";
 const BillProcessStepThree = () => {
@@ -14,15 +14,34 @@ const BillProcessStepThree = () => {
 
   const navigate = useNavigate();
 
+  const { UserMangementReducer } = useSelector((state) => state);
+
+  const organizationName = localStorage.getItem("OrganizatioName");
   //States
   const [getAllPakagesData, setGetAllPakagesData] = useState([]);
 
   //UseEffect For Get All Organziation Selected Pakages
 
   useEffect(() => {
-    let data = {};
+    let data = { OrganizationName: organizationName };
     dispatch(getOrganizationSelectedPakagesAPI(navigate, t, data));
   }, []);
+
+  //excreting the data for  Get All Organziation Selected Pakages
+
+  useEffect(() => {
+    if (
+      UserMangementReducer.getAllSelectedPakagesData !== null &&
+      UserMangementReducer.getAllSelectedPakagesData !== undefined
+    ) {
+      setGetAllPakagesData(
+        UserMangementReducer.getAllSelectedPakagesData
+          .organizationSelectedPackages
+      );
+    }
+  }, [UserMangementReducer.getAllSelectedPakagesData]);
+
+  console.log(getAllPakagesData, "getAllSelectedPakagesData");
 
   const ColumnsPakageSelection = [
     {
@@ -32,9 +51,17 @@ const BillProcessStepThree = () => {
         </span>
       ),
       width: 100,
-      dataIndex: "Pakagedetails",
-      key: "Pakagedetails",
+      dataIndex: "name",
+      key: "name",
       align: "center",
+      render: (text, record) => {
+        console.log(record, "renderrender");
+        return (
+          <>
+            <span className={styles["Tableheading"]}>{record.name}</span>
+          </>
+        );
+      },
     },
     {
       title: (
@@ -42,10 +69,19 @@ const BillProcessStepThree = () => {
           {t("Charges-per-license-US$")}
         </span>
       ),
-      dataIndex: "Chargesperlicense",
-      key: "Chargesperlicense",
+      dataIndex: "price",
+      key: "price",
       width: 100,
       align: "center",
+      render: (text, record) => {
+        return (
+          <>
+            <span className={styles["ChargesPerLicesense"]}>
+              {record.price}
+            </span>
+          </>
+        );
+      },
     },
     {
       title: (
@@ -54,9 +90,18 @@ const BillProcessStepThree = () => {
         </span>
       ),
       width: 100,
-      dataIndex: "Numberoflicenses",
-      key: "Numberoflicenses",
+      dataIndex: "headCount",
+      key: "headCount",
       align: "center",
+      render: (text, record) => {
+        return (
+          <>
+            <span className={styles["ChargesPerLicesense"]}>
+              {record.headCount}
+            </span>
+          </>
+        );
+      },
     },
     {
       title: (
@@ -68,78 +113,50 @@ const BillProcessStepThree = () => {
       key: "Yearlycharges",
       align: "center",
       width: 100,
-    },
-  ];
-  const Data = [
-    {
-      Pakagedetails: (
-        <span className={styles["Tableheading"]}>{t("Essential")}</span>
-      ),
-      Chargesperlicense: (
-        <>
-          <span className={styles["ChargesPerLicesense"]}>25</span>
-        </>
-      ),
-      Numberoflicenses: (
-        <>
-          <span className={styles["ChargesPerLicesense"]}>12</span>
-        </>
-      ),
-
-      Yearlycharges: (
-        <>
-          <span className={styles["ChargesPerLicesense"]}>1,024</span>
-        </>
-      ),
-    },
-    {
-      Pakagedetails: (
-        <span className={styles["Tableheading"]}>{t("Professional")}</span>
-      ),
-      Chargesperlicense: (
-        <>
-          <span className={styles["ChargesPerLicesense"]}>35</span>
-        </>
-      ),
-      Numberoflicenses: (
-        <>
-          <span className={styles["ChargesPerLicesense"]}>35</span>
-        </>
-      ),
-
-      Yearlycharges: (
-        <>
-          <span className={styles["ChargesPerLicesense"]}>875</span>
-        </>
-      ),
-    },
-    {
-      Pakagedetails: (
-        <span className={styles["Tableheading"]}>{t("Premium")}</span>
-      ),
-      Chargesperlicense: (
-        <>
-          <span className={styles["ChargesPerLicesense"]}>45</span>
-        </>
-      ),
-      Numberoflicenses: (
-        <span className={styles["ChargesPerLicesense"]}>45</span>
-      ),
-      Yearlycharges: (
-        <>
-          <span className={styles["ChargesPerLicesense"]}>9,024</span>
-        </>
-      ),
+      render: (text, record) => {
+        if (record.name === "Total") {
+          // For the total row, directly use the calculated value
+          return (
+            <span className={styles["ChargesPerLicesense"]}>
+              {record.Yearlycharges.toLocaleString()}
+            </span>
+          );
+        } else {
+          // For regular rows, calculate the yearly charges
+          const yearlyCharge = (record.price || 0) * 12;
+          return (
+            <span className={styles["ChargesPerLicesense"]}>
+              {yearlyCharge.toLocaleString()}
+            </span>
+          );
+        }
+      },
     },
   ];
 
-  const defaultRow = {
-    Pakagedetails: <span className={styles["TableheadingTotal"]}>Total</span>,
-    Numberoflicenses: <span className={styles["ChargesPerLicesense"]}>43</span>,
-    Yearlycharges: (
-      <span className={styles["ChargesPerLicesense"]}>13,072</span>
-    ),
+  const calculateTotals = (data) => {
+    // Calculate total licenses (which seems to be correctly calculated in your provided code).
+    const totalLicenses = data.reduce(
+      (acc, cur) => acc + (Number(cur.headCount) || 0),
+      0
+    );
+
+    // Calculate total yearly charges.
+    const totalYearlyCharges = data.reduce(
+      (acc, cur) => acc + (Number(cur.price) || 0) * cur.headCount * 12,
+      0
+    );
+
+    // Return an object with the totals that can be used as a row in your table.
+    return {
+      name: "Total",
+      headCount: totalLicenses,
+      Yearlycharges: totalYearlyCharges.toLocaleString(), // Convert to a string with thousand separators.
+    };
   };
+
+  const totalRow = calculateTotals(getAllPakagesData);
+
   return (
     <Container>
       <Row>
@@ -155,7 +172,7 @@ const BillProcessStepThree = () => {
               <TableToDo
                 column={ColumnsPakageSelection}
                 className={"Billing_TablePakageSelection"}
-                rows={[...Data, defaultRow]}
+                rows={[...getAllPakagesData, totalRow]}
                 pagination={false}
                 id="PakageDetails"
                 rowHoverBg="none"
