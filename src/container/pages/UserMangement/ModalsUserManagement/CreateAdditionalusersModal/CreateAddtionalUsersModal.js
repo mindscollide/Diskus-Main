80b8b047ce3a28c8748da45db977f1f14298893c
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2024-04-04 13:00:23 +0500

    code push

--- src/container/pages/UserMangement/ModalsUserManagement/CreateAdditionalusersModal/CreateAddtionalUsersModal.js
+++ src/container/pages/UserMangement/ModalsUserManagement/CreateAdditionalusersModal/CreateAddtionalUsersModal.js
@@ -15,7 +15,6 @@ import {
 } from "../../../../../components/elements";
 import crossicon from "../../../../../assets/images/BlackCrossIconModals.svg";
 import { Col, Row } from "react-bootstrap";
-import { style } from "@material-ui/system";
 import EmployeeinfoCard from "../../../../../components/elements/Employeeinfocard/EmployeeinfoCard";
 const CreateAddtionalUsersModal = () => {
   const { t } = useTranslation();
@@ -23,28 +22,133 @@ const CreateAddtionalUsersModal = () => {
 
   const { UserManagementModals } = useSelector((state) => state);
 
-  const [members, setMembers] = useState([
-    {
-      name: "Saif ul islam",
-      emailAddress: "saifiiyousuf4002@gmail.com",
+  //States
+  const [members, setMembers] = useState([]);
+  const [createAddionalUsers, setCreateAddionalUsers] = useState({
+    Name: {
+      value: "",
+      errorMessage: "",
+      errorStatus: false,
     },
-    {
-      name: "huzaifa jahangir",
-      emailAddress: "saifiiyousuf4002@gmail.com",
-    },
-    {
-      name: "Owais",
-      emailAddress: "saifiiyousuf4002@gmail.com",
-    },
-    {
-      name: "mamdani",
-      emailAddress: "saifiiyousuf4002@gmail.com",
-    },
-  ]);
 
-  const handleCrossIcon = () => {
+    Email: {
+      value: "",
+      errorMessage: "",
+      errorStatus: false,
+    },
+    Designation: {
+      value: "",
+      errorMessage: "",
+      errorStatus: false,
+    },
+  });
+
+  //OnChange For Text Fields
+  const createAddiotionalUsersHandler = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    if (name === "Name" && value !== "") {
+      setCreateAddionalUsers({
+        ...createAddionalUsers,
+        Name: {
+          value: value.trimStart(),
+          errorMessage: "",
+          errorStatus: false,
+        },
+      });
+    } else if (name === "Name" && value === "") {
+      setCreateAddionalUsers({
+        ...createAddionalUsers,
+        Name: {
+          value: "",
+          errorMessage: "",
+          errorStatus: false,
+        },
+      });
+    }
+
+    if (name === "Email" && value !== "") {
+      if (value !== "") {
+        setCreateAddionalUsers({
+          ...createAddionalUsers,
+          Email: {
+            value: value.trimStart(),
+            errorMessage: "",
+            errorStatus: false,
+          },
+        });
+      }
+    } else if (name === "Email" && value === "") {
+      setCreateAddionalUsers({
+        ...createAddionalUsers,
+        Email: {
+          value: "",
+          errorMessage: "",
+          errorStatus: false,
+        },
+      });
+    }
+
+    if (name === "Designation" && value !== "") {
+      if (value !== "") {
+        setCreateAddionalUsers({
+          ...createAddionalUsers,
+          Designation: {
+            value: value.trimStart(),
+            errorMessage: "",
+            errorStatus: false,
+          },
+        });
+      }
+    } else if (name === "Designation" && value === "") {
+      setCreateAddionalUsers({
+        ...createAddionalUsers,
+        Designation: {
+          value: "",
+          errorMessage: "",
+          errorStatus: false,
+        },
+      });
+    }
+  };
+
+  //Adding Addtional Users
+  const handleAddMember = () => {
+    const { Name, Email } = createAddionalUsers;
+    if (Name.value && Email.value) {
+      // Add member to the members array
+      setMembers((prevMembers) => [
+        ...prevMembers,
+        { name: Name.value, email: Email.value },
+      ]);
+
+      // Reset the input fields
+      setCreateAddionalUsers({
+        ...createAddionalUsers,
+        Name: { ...Name, value: "" },
+        Email: { ...Email, value: "" },
+      });
+    } else {
+      // Optionally handle the case where Name or Email is empty
+      // e.g., set error messages in createAddionalUsers
+    }
+  };
+
+  //handle removing the addional Users
+  const handleCrossIcon = (indexToRemove) => {
+    setMembers((currentMembers) =>
+      currentMembers.filter((_, index) => index !== indexToRemove)
+    );
+  };
+
+  //Handle Skip Button
+  const handleSkipButton = () => {
     dispatch(showCreateAddtionalUsersModal(false));
   };
+
+  //handle Create button
+  const handleCreatebutton = () => {};
+
   return (
     <section>
       <Modal
@@ -54,28 +158,8 @@ const CreateAddtionalUsersModal = () => {
         modalHeaderClassName={"d-block"}
         size={"md"}
         onHide={() => {
-          dispatch(showCreateAddtionalUsersModal(false));
+          dispatch(showCreateAddtionalUsersModal(true));
         }}
-        ModalTitle={
-          <>
-            {" "}
-            <Row>
-              <Col
-                lg={12}
-                md={12}
-                sm={12}
-                className="d-flex justify-content-end align-items-center"
-              >
-                <img
-                  src={crossicon}
-                  alt=""
-                  onClick={handleCrossIcon}
-                  className="cursor-pointer"
-                />
-              </Col>
-            </Row>
-          </>
-        }
         ModalBody={
           <>
             <section className={styles["Padding_Alignment"]}>
@@ -87,23 +171,18 @@ const CreateAddtionalUsersModal = () => {
                 </Col>
               </Row>
               <Row className="mt-1">
-                <Col lg={2} md={2} sm={12} xs={12}></Col>
-                <Col
-                  lg={8}
-                  md={8}
-                  sm={12}
-                  xs={12}
-                  className={styles["RedSrtip"]}
-                >
+                <Col lg={12} md={12} sm={12} className={styles["RedSrtip"]}>
                   <span className={styles["RedStripContent"]}>
                     {t("Maximum-20-users-can-be-created-in-trial-version")}
                   </span>
                 </Col>
-                <Col lg={2} md={2} sm={12} xs={12}></Col>
               </Row>
               <Row className="mt-3">
-                <Col lg={10} md={10} sm={12} xs={12}>
+                <Col lg={12} md={12} sm={12} xs={12}>
                   <TextField
+                    name="Name"
+                    value={createAddionalUsers.Name.value}
+                    change={createAddiotionalUsersHandler}
                     placeholder={t("Full-name")}
                     label={
                       <>
@@ -120,6 +199,73 @@ const CreateAddtionalUsersModal = () => {
                     applyClass={"updateNotes_titleInput"}
                   />
                 </Col>
+              </Row>
+
+              <Row className="mt-3">
+                <Col lg={6} md={6} sm={12}>
+                  <TextField
+                    placeholder={t("Email")}
+                    label={
+                      <>
+                        <span className={styles["NameCreateAddtional"]}>
+                          {t("Email")}{" "}
+                          <span className={styles["Steric"]}>*</span>
+                        </span>
+                      </>
+                    }
+                    applyClass={"updateNotes_titleInput"}
+                    name="Email"
+                    value={createAddionalUsers.Email.value}
+                    change={createAddiotionalUsersHandler}
+                  />
+                </Col>{" "}
+                <Col lg={6} md={6} sm={12}>
+                  <TextField
+                    placeholder={t("Designation")}
+                    label={
+                      <>
+                        <span className={styles["NameCreateAddtional"]}>
+                          {t("Designation")}{" "}
+                          <span className={styles["Steric"]}>*</span>
+                        </span>
+                      </>
+                    }
+                    applyClass={"updateNotes_titleInput"}
+                  />
+                </Col>
+              </Row>
+              <Row className="mt-4">
+                <Col
+                  lg={5}
+                  md={5}
+                  sm={12}
+                  xs={12}
+                  className="d-flex flex-column flex-wrap"
+                >
+                  <span className={styles["NameCreateAddtional"]}>
+                    {t("Organization")}
+                  </span>
+                  <span className={styles["NameClass"]}>Waqas Associates</span>
+                </Col>
+                <Col
+                  lg={5}
+                  md={5}
+                  sm={12}
+                  xs={12}
+                  className="flex-column flex-wrap"
+                >
+                  <span className={styles["NameCreateAddtional"]}>
+                    {t("Select-role")}
+                  </span>
+                  <Row>
+                    <Col lg={12} md={12} sm={12} className="d-flex gap-2">
+                      <Checkbox classNameCheckBoxP="m-0 p-0" classNameDiv="" />
+                      <span className={styles["AdminAlsoClass"]}>
+                        {t("Is-admin-also")}
+                      </span>
+                    </Col>
+                  </Row>
+                </Col>{" "}
                 <Col
                   lg={2}
                   md={2}
@@ -128,6 +274,7 @@ const CreateAddtionalUsersModal = () => {
                   className={styles["buttonStyles"]}
                 >
                   <Button
+                    onClick={handleAddMember}
                     text={
                       <>
                         <Row>
@@ -153,62 +300,6 @@ const CreateAddtionalUsersModal = () => {
               </Row>
               <Row className="mt-3">
                 <Col
-                  lg={6}
-                  md={6}
-                  sm={12}
-                  className="d-flex flex-column flex-wrap"
-                >
-                  <span className={styles["NameCreateAddtional"]}>
-                    {t("Organization")}
-                  </span>
-                  <span className={styles["NameClass"]}>Waqas Associates</span>
-                </Col>
-                <Col lg={6} md={6} sm={12} className="flex-column flex-wrap">
-                  <span className={styles["NameCreateAddtional"]}>
-                    {t("Select-role")}
-                  </span>
-                  <Row>
-                    <Col lg={12} md={12} sm={12} className="d-flex gap-2">
-                      <Checkbox classNameCheckBoxP="m-0 p-0" classNameDiv="" />
-                      <span className={styles["AdminAlsoClass"]}>
-                        {t("Is-admin-also")}
-                      </span>
-                    </Col>
-                  </Row>
-                </Col>
-              </Row>
-              <Row>
-                <Col lg={6} md={6} sm={12}>
-                  <TextField
-                    placeholder={t("Email")}
-                    label={
-                      <>
-                        <span className={styles["NameCreateAddtional"]}>
-                          {t("Email")}{" "}
-                          <span className={styles["Steric"]}>*</span>
-                        </span>
-                      </>
-                    }
-                    applyClass={"updateNotes_titleInput"}
-                  />
-                </Col>{" "}
-                <Col lg={6} md={6} sm={12}>
-                  <TextField
-                    placeholder={t("Designation")}
-                    label={
-                      <>
-                        <span className={styles["NameCreateAddtional"]}>
-                          {t("Designation")}{" "}
-                          <span className={styles["Steric"]}>*</span>
-                        </span>
-                      </>
-                    }
-                    applyClass={"updateNotes_titleInput"}
-                  />
-                </Col>
-              </Row>
-              <Row className="mt-3">
-                <Col
                   lg={12}
                   md={12}
                   sm={12}
@@ -217,27 +308,32 @@ const CreateAddtionalUsersModal = () => {
                 >
                   <Row>
                     {members.map((data, index) => {
+                      console.log(data, "membersmembersmembers");
                       return (
-                        <Col lg={6} md={6} sm={12} className="mt-2">
-                          <Row>
-                            <Col lg={12} md={12} sm={12}>
-                              <EmployeeinfoCard
-                                Employeename={data?.name}
-                                Employeeemail={data?.emailAddress}
-                                EmployeePic={data?.displayProfilePictureName}
-                                Icon={
-                                  <img
-                                    src={CrossIcon}
-                                    width="18px"
-                                    height="18px"
-                                    alt=""
-                                    draggable="false"
-                                  />
-                                }
-                              />
-                            </Col>
-                          </Row>
-                        </Col>
+                        <>
+                          <Col lg={6} md={6} sm={12} className="mt-2">
+                            <Row>
+                              <Col lg={12} md={12} sm={12}>
+                                <EmployeeinfoCard
+                                  Employeename={data?.name}
+                                  Employeeemail={data?.email}
+                                  EmployeePic={data?.displayProfilePictureName}
+                                  Icon={
+                                    <img
+                                      src={CrossIcon}
+                                      width="18px"
+                                      height="18px"
+                                      alt=""
+                                      draggable="false"
+                                      className="cursor-pointer"
+                                      onClick={() => handleCrossIcon(index)}
+                                    />
+                                  }
+                                />
+                              </Col>
+                            </Row>
+                          </Col>
+                        </>
                       );
                     })}
                   </Row>
@@ -259,11 +355,13 @@ const CreateAddtionalUsersModal = () => {
                   <Button
                     text={t("Skip")}
                     className={styles["SkipandResetButtonClass"]}
+                    onClick={handleSkipButton}
                   />
 
                   <Button
                     text={t("Create")}
                     className={styles["CreateButtonClass"]}
+                    onClick={handleCreatebutton}
                   />
                 </Col>
               </Row>
