20cbc174ba0f7c5d0ad473eb47c8bb0a71102531
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2024-04-09 09:15:51 +0500

    calculator completed

--- src/container/pages/UserMangement/PakageDetailsUserManagement/PakageDetailsUserManagement.js
+++ src/container/pages/UserMangement/PakageDetailsUserManagement/PakageDetailsUserManagement.js
@@ -207,7 +207,9 @@ const PakageDetailsUserManagement = () => {
           );
         } else {
           if (row.isTotalRow) {
-            return;
+            return (
+              <span className={styles["ChargesPerLicesense"]}>{text}</span>
+            );
           } else {
             return (
               <>
@@ -252,7 +254,9 @@ const PakageDetailsUserManagement = () => {
           );
         } else {
           if (row.isTotalRow) {
-            return;
+            return (
+              <span className={styles["ChargesPerLicesense"]}>{text}</span>
+            );
           } else {
             return (
               <>
@@ -274,8 +278,8 @@ const PakageDetailsUserManagement = () => {
           </span>
         </span>
       ),
-      dataIndex: "Yearlycharges",
-      key: "Yearlycharges",
+      dataIndex: "YearlychargesTotal",
+      key: "YearlychargesTotal",
       align: "center",
       width: 100,
       render: (text, row) => {
@@ -295,7 +299,9 @@ const PakageDetailsUserManagement = () => {
           );
         } else {
           if (row.isTotalRow) {
-            return;
+            return (
+              <span className={styles["ChargesPerLicesense"]}>{text}</span>
+            );
           } else {
             return (
               <>
@@ -323,15 +329,34 @@ const PakageDetailsUserManagement = () => {
 
   //Calculating the totals
   const calculateTotals = (data) => {
-    const totalLicenses = tableData.reduce(
+    const totalLicenses = data.reduce(
       (total, row) => total + (Number(row.licenseCount) || 0),
       0
     );
+
+    // Calculate total monthly charges
+    const totalMonthlyCharges = data.reduce((total, row) => {
+      const monthlyCharge = row.price * (Number(row.licenseCount) || 0);
+      return total + monthlyCharge;
+    }, 0);
+
+    const totalQuarterlyCharges = data.reduce((total, row) => {
+      const quarterlyCharge = row.price * (Number(row.licenseCount) || 0) * 3; // Multiply by 3 for quarterly
+      return total + quarterlyCharge;
+    }, 0);
+
+    const totalYearlyCharges = data.reduce((total, row) => {
+      const yearlyCharge = row.price * (Number(row.licenseCount) || 0) * 12; // Multiply by 12 for yearly
+      return total + yearlyCharge;
+    }, 0);
 
     // Return an object with the totals that can be used as a row in your table.
     return {
       name: "Total",
       Numberoflicenses: totalLicenses,
+      price: totalMonthlyCharges,
+      Quarterlycharges: totalQuarterlyCharges,
+      YearlychargesTotal: totalYearlyCharges,
     };
   };
 
