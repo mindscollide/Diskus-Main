dbf8bf286d6feff3fb38ab91580e164afde9d705
Owais Wajid Khan <owaiswajidkhan@gmail.com>
2024-05-02 11:36:56 +0500

    Review Minutes Accepted

--- src/container/MinutesNewFlow/reviewMinutes/ReviewMinutes.js
+++ src/container/MinutesNewFlow/reviewMinutes/ReviewMinutes.js
@@ -493,7 +493,96 @@ const ReviewMinutes = () => {
                       </div>
                     </Col>
                   </Row>
-
+                  <Row>
+                    <Col lg={12} md={12} sm={12} className="position-relative">
+                      <div
+                        className={styles["version-control-wrapper-with-more"]}
+                      >
+                        <span className={styles["with-text"]}>V1.0</span>
+                      </div>
+                      <div className={styles["uploaded-details-accepted"]}>
+                        <Row className={styles["inherit-height"]}>
+                          <Col lg={8} md={8} sm={12}>
+                            <p className={styles["minutes-text"]}>
+                              Task updates: Design phase completed, moving to
+                              development, discussed resource reallocation to
+                              address delays and decided unknown unknown printer
+                              took a galley of type a printer took a galley of
+                              type a to hold daily check-ins for quicker
+                              progress Design phase completed, moving to
+                              development, discussed resource reallocation to
+                              address delays and decided unknown unknown printer
+                              took a galley of type a printer took a galley of
+                              type a to hold daily check-ins for quicker
+                              progress Design phase completed, moving to
+                              development, discussed resource reallocation to
+                              address delays and decided unknown unknown printer
+                              took a galley of type a printer took a galley of
+                              type a to update.
+                            </p>
+                          </Col>
+                          <Col
+                            lg={4}
+                            md={4}
+                            sm={12}
+                            className="position-relative"
+                          >
+                            <Row className="m-0">
+                              <Col lg={6} md={6} sm={12} className="p-0">
+                                <span className={styles["bar-line"]}></span>
+                                <p className={styles["uploadedbyuser"]}>
+                                  Uploaded By
+                                </p>
+                                <div className={styles["gap-ti"]}>
+                                  <img
+                                    src={DefaultAvatar}
+                                    className={styles["Image"]}
+                                    alt=""
+                                    draggable={false}
+                                  />
+                                  <p className={styles["agendaCreater"]}>
+                                    Alex Rodriguez
+                                  </p>
+                                </div>
+                              </Col>
+                              <Col
+                                lg={6}
+                                md={6}
+                                sm={12}
+                                className={
+                                  currentLanguage === "ar"
+                                    ? "text-start p-0"
+                                    : "text-end p-0"
+                                }
+                              >
+                                <Button
+                                  text={t("Accepted")}
+                                  className={styles["Accepted-comment"]}
+                                />
+                                <Button
+                                  text={t("Reject")}
+                                  className={styles["Reject-comment"]}
+                                  onClick={() =>
+                                    dispatch(rejectCommentModal(true))
+                                  }
+                                />
+                              </Col>
+                            </Row>
+                            <Row>
+                              <Col lg={12} md={12} sm={12}>
+                                <p className={styles["time-uploader"]}>
+                                  4:00pm,
+                                </p>
+                                <p className={styles["date-uploader"]}>
+                                  18th May, 2024
+                                </p>
+                              </Col>
+                            </Row>
+                          </Col>
+                        </Row>
+                      </div>
+                    </Col>
+                  </Row>
                   <Row>
                     <Col lg={12} md={12} sm={12} className="position-relative">
                       <div className={styles["version-control-wrapper"]}>
