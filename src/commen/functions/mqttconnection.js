abe6578c0d07512d8ee3be605f049fac1811f683
Ali Raza Mamdani <Ali.mamdani@hotmail.com>
2024-08-12 14:42:17 +0500

    Update mqttconnection.js

--- src/commen/functions/mqttconnection.js
+++ src/commen/functions/mqttconnection.js
@@ -3,16 +3,18 @@ import { useEffect } from "react";
 import { setClient } from "../../store/actions/Auth2_actions";
 import Helper from "./history_logout";
 let newClient;
+
 export const onConnected = (newClient, subscribeID) => {
   console.log("Connected to MQTT broker onConnected");
-  newClient.subscribe(subscribeID.toString());
-  // localStorage.setItem('MqttConnectionState', true)
+  if (newClient.isConnected()) {
+    newClient.subscribe(subscribeID.toString());
+  } else {
+    console.log("WebSocket is not in OPEN state for subscription.");
+  }
 };
-
 export const onConnectionLost = (subscribeID) => {
   console.log("Connected to MQTT broker onConnectionLost");
   setTimeout(mqttConnection(subscribeID), 3000);
-  // localStorage.setItem('MqttConnectionState', false)
 };
 
 export const mqttConnection = (subscribeID) => {
@@ -20,8 +22,12 @@ export const mqttConnection = (subscribeID) => {
   console.log("mqtt resquest ");
   var max = 90000;
   var id = min + Math.random() * (max - min);
-  const brokerUrl = "wss://portal.letsdiskus.com:8883/mqtt";
-  newClient = new Paho.Client(brokerUrl, parseInt(subscribeID) + "-" + id);
+
+  newClient = new Paho.Client(
+    "192.168.18.241",
+    8228,
+    parseInt(subscribeID) + "-" + id
+  );
 
   var options = {
     onSuccess: () => {
