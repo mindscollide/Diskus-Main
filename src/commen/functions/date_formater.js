6399d1b09f9e662dd22582258e446d0d61a424ba
Ali Raza Mamdani <Ali.mamdani@hotmail.com>
2024-09-06 13:17:31 +0500

    code push

--- src/commen/functions/date_formater.js
+++ src/commen/functions/date_formater.js
@@ -773,6 +773,24 @@ export const newTimeFormaterForImportMeetingAgenda = (dateTime) => {
   return moment(_dateTime).format("h:mm A - D MMM, YYYY");
 };
 
+export const LoginHistoryReport = (dateTime) => {
+  let fullDateyear =
+  dateTime?.slice(0, 4) +
+  "-" +
+  dateTime?.slice(4, 6) +
+  "-" +
+  dateTime?.slice(6, 8) +
+  "T" +
+  dateTime?.slice(8, 10) +
+  ":" +
+  dateTime?.slice(10, 12) +
+  ":" +
+  dateTime?.slice(12, 14) +
+  ".000Z";
+let _dateTime = new Date(fullDateyear).toString("YYYYMMDDHHmmss");
+return moment(_dateTime).format("MMM dd, yyyy HH:mm");
+}
+
 //Converting to GMT Having Date Plus StartTime And End Time
 export const convertDateTimeRangeToGMT = (startTime, endTime) => {
   let StartTimeFormat =
