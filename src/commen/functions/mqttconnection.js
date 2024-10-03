c557b1554f220767b277a0a533308591bfe54776
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2024-10-03 12:16:37 +0500

    mqttConnection.js regex.js time_formatter.js

--- src/commen/functions/mqttconnection.js
+++ src/commen/functions/mqttconnection.js
@@ -1,5 +1,4 @@
 import Paho from "paho-mqtt";
-import { useEffect } from "react";
 import { setClient } from "../../store/actions/Auth2_actions";
 import Helper from "./history_logout";
 
@@ -22,8 +21,6 @@ export const mqttConnection = (subscribeID) => {
     console.log("Connection lost:", responseObject.errorMessage);
     setTimeout(() => mqttConnection(subscribeID), 3000); // Reconnect after 3 seconds
   };
-
-  // newClient.onMessageArrived = onMessageArrived;
 
   const options = {
     onSuccess: () => {
