e78cd7e9d92b0f38f43beb1eab3ab0978bb74175
Ali Raza Mamdani <Ali.mamdani@hotmail.com>
2024-10-16 17:08:20 +0500

    Update index.js

--- src/components/elements/snack_bar/index.js
+++ src/components/elements/snack_bar/index.js
@@ -1,50 +1,49 @@
 import React from "react";
 import PropTypes from "prop-types"; // For prop type validation
-import Snackbar from '@mui/material/Snackbar';
-import MuiAlert from '@mui/material/Alert';
-import { Safe } from 'react-bootstrap-icons'
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import { ExclamationDiamond, Airplane } from "react-bootstrap-icons";
 
-const Alert = React.forwardRef(function Alert(props, ref, alertClass) {
-  return (
-    <MuiAlert
-      elevation={6}
-      ref={ref}
-      className={alertClass}
-      variant="filled"
-      {...props}
-      icon={<Safe />}
-    />
-  );
-});
+const Notification = React.memo(
+  ({ setOpen, open, message, severity = "success" }) => {
+    const handleClose = (event, reason) => {
+      if (reason === "clickaway") {
+        return;
+      }
+      setOpen(false);
+    };
 
-const Notification = React.memo(({ setOpen, open, message, severity = "success" }) => {
-  const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-    setOpen(false);
-  };
+    return (
+      <Snackbar
+        open={true}
+        // className={severity === "success"? "success_snackbar": "error_snackbar"}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        autoHideDuration={6000}
+        onClose={handleClose}>
+        <MuiAlert
+          elevation={6}
+          icon={severity === "success" ? <Airplane /> : <ExclamationDiamond />}
+          style={{ alignItems: "center" }}
+          sx={{
+            width: "100%",
+            backgroundColor: severity === "success" ? "#6172d6" : "#ce0000",
+          }} // Customize background colors
+          variant='filled'>
+          {message}
+        </MuiAlert>
 
-  return (
-    <Snackbar 
-      open={true} 
-      // className={severity === "success"? "success_snackbar": "error_snackbar"}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      autoHideDuration={6000} 
-      onClose={handleClose}
-      
-       
-    >
-      <Alert 
+        {/* <Alert 
         onClose={handleClose} 
+        c
         severity={severity} // Use dynamic severity
-        sx={{ width: '100%', backgroundColor: severity === "success" ?  "#6172d6" : "#ce0000" }} // Customize background colors
+        
       >
         {message}
-      </Alert>
-    </Snackbar>
-  );
-});
+      </Alert> */}
+      </Snackbar>
+    );
+  }
+);
 
 // Prop type validation
 Notification.propTypes = {
