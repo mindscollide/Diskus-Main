98d2ee0ed36d84c447c070eda920de284577786f
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2024-11-18 19:29:32 +0500

    Rubish code removed

--- src/container/pages/meeting/viewMeetings/Minutes/Minutes.js
+++ src/container/pages/meeting/viewMeetings/Minutes/Minutes.js
@@ -203,18 +203,6 @@ const Minutes = ({
         GetAllGeneralMinutesApiFunc(navigate, t, Data, advanceMeetingModalID)
       );
 
-      // dispatch(
-      //   GetAllAgendaWiseMinutesApiFunc(
-      //     navigate,
-      //     Data,
-      //     t,
-      //     Number(advanceMeetingModalID),
-      //     false,
-      //     false,
-      //     true
-      //   )
-      // );
-
       dispatch(GetMinuteReviewStatsForOrganizerByMeetingId(Data2, navigate, t));
     }
 
