b6ff217d6d5a0b5d396de994e5b17a8009c775b1
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2024-12-24 17:21:11 +0500

    Agenda Contributor view meeting mode fixed

--- src/components/layout/WebNotfication/WebNotfication.js
+++ src/components/layout/WebNotfication/WebNotfication.js
@@ -478,7 +478,29 @@ const WebNotfication = ({
       }
     } else if (NotificationData.notificationActionID === 11) {
       if (currentURL.includes("/Diskus/Meeting")) {
-        return; // Perform no action if the URL matches
+        if (PayLoadData.IsQuickMeeting === true) {
+          let Data = { MeetingID: Number(PayLoadData.MeetingID) };
+          dispatch(
+            ViewMeeting(navigate, Data, t, setViewFlag, false, false, 6)
+          );
+        } else {
+          localStorage.setItem("AdvanceMeetingOperations", true);
+          localStorage.setItem(
+            "NotificationAdvanceMeetingID",
+            PayLoadData.MeetingID
+          );
+          let Data = { MeetingID: Number(PayLoadData.MeetingID) };
+          dispatch(
+            GetMeetingStatusDataAPI(
+              navigate,
+              t,
+              Data,
+              setEditorRole,
+              true,
+              setViewAdvanceMeetingModal
+            )
+          );
+        }
       } else {
         //Notification For Added as An Agenda Contributor
         if (PayLoadData.IsQuickMeeting === true) {
