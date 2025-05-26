d10f226bcb3903eda6714fdfbad5895f0b89660b
Huzeifa Jahangir <huzeifajahangir@gmail.com>
2025-05-26 15:08:03 +0500

    changes

--- src/store/reducers/Setting_reducer.js
+++ src/store/reducers/Setting_reducer.js
@@ -37,7 +37,7 @@ const initialState = {
   webNotificationDataVideoIntimination: null,
   globalFunctionWebnotificationFlag: false,
   googleClientID: null,
-  microsoftClientID: "",
+  microsoftClientID: null,
 };
 
 const settingReducer = (state = initialState, action) => {
