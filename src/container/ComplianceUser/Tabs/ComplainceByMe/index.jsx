68cbeda26684f5583c34e52a71fffcb3a9f6b67b
Ali Raza Mamdani <Ali.mamdani@hotmail.com>
2026-01-01 18:28:34 +0500

    Add compliance table UI and styling for ComplianceByMe
    Introduced a compliance table in ComplianceByMe with sample data, custom columns, and action buttons. Added new CSS module for button styling and updated Table component to support sticky headers and fixed layout. Enhanced Table.css for Compliance_Table appearance.

--- src/container/ComplianceUser/Tabs/ComplainceByMe/index.jsx
+++ src/container/ComplianceUser/Tabs/ComplainceByMe/index.jsx
@@ -1,7 +1,97 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
+import CustomTable from "../../../../components/elements/table/Table";
+import CustomButton from "../../../../components/elements/button/Button";
+import styles from './complianceByMe.module.css'
+
 
 const ComplianceByMe = () => {
-  return <div>ComplianceByMe</div>;
+  const [rows, setRows] = useState([
+    {
+      ComplianceTitle:
+        "Implementation of End-to-End Data Encryption Across All Intern",
+      Criticality: "High",
+      Status: "In Progress",
+      DueDate: "07 May 2025",
+      Authority: "PSX",
+    },
+    {
+      ComplianceTitle: "Data Protection Policy",
+      Criticality: "High",
+      Status: "In Progress",
+      DueDate: "2024-12-31",
+      Authority: "John Doe",
+    },
+  ]);
+  const columns = useMemo(
+    () => [
+      {
+        title: "Compliance Title",
+        dataIndex: "ComplianceTitle",
+        key: "ComplianceTitle",
+        width: 350,
+        ellipsis: true,
+        align: "left",
+        // render: (text, record) => (
+        //   <span>
+        //     Implementation of End-to-End Data Encryption Across All Intern...
+        //   </span>
+        // ),
+      },
+      {
+        title: "Criticality",
+        dataIndex: "Criticality",
+        key: "Criticality",
+        width: 150,
+        align: "center",
+      },
+      {
+        title: "Status",
+        dataIndex: "Status",
+        key: "Status",
+        width: 150,
+        align: "center",
+      },
+      {
+        title: "Due Date",
+        dataIndex: "DueDate",
+        key: "DueDate",
+        width: 200,
+        align: "center",
+      },
+      {
+        title: "Authority",
+        dataIndex: "Authority",
+        key: "Authority",
+        width: 200,
+        align: "center",
+      },
+      {
+        title: "",
+        dataIndex: "Authority",
+        key: "Authority",
+        render: (_, record) => {
+          return (
+            <div className='d-flex align-item-center justify-content-center gap-2'>
+              <CustomButton className={styles["actionButtons_complianceList"]} text={"Edit"} />
+              <CustomButton  className={styles["actionButtons_complianceList"]} text={"View Details"} />
+            </div>
+          );
+        },
+      },
+    ],
+    []
+  );
+  return (
+    <>
+      <CustomTable
+        rows={rows}
+        column={columns}
+        className={"Compliance_Table mt-3"}
+        scroll={{ x: "max-content", y: 550 }}
+        pagination={false}
+      />{" "}
+    </>
+  );
 };
 
 export default ComplianceByMe;
